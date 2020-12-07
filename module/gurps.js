c1b9c704cb9922bde222aabf7543930de7766952
crnormand <crnormand@bellsouth.net>
2020-12-07 06:08:22 -0500

    Tweaked NPC layout, diving deep into stat block parsing

--- module/gurps.js
+++ module/gurps.js
@@ -105,6 +105,37 @@ GURPS.woundModifiers = {
 	"dmg": { multiplier: 1, label: 'Damage' }
 };
 
+GURPS.damageTypeMap = {
+	"burn":"burn",
+	"cor":"cor",
+	"cr":"cr",
+	"cut":"cut",
+	"fat":"fat",
+	"imp":"imp",
+	"pi-":"pi-",
+	"pi":"pi",
+	"pi+":"pi+",
+	"pi++":"pi++",
+	"toxic": "tox",
+	"burning":"burn",
+	"corrosion":"cor",
+	"corrosive":"cor",
+	"crush":"cr",
+	"crushing":"cr",
+	"cutting":"cut",
+	"fatigue":"fat",
+	"impaling":"imp",
+	"small piercing":"pi-",
+	"piercing":"pi",
+	"large piercing":"pi+",
+	"huge piercing":"pi++",
+	"toxic": "tox",
+};	
+
+
+GURPS.parseDmg = (dmg) => { return dmg.replace(/^(\d+)d([-+]\d+)?([xX\*]\d+)? ?(\([.\d]+\))?(!)? ?(.*)$/g, "$1~$2~$3~$4~$5~$6") };
+
+
 GURPS.attributepaths = {
 	"ST": "attributes.ST.value",
 	"DX": "attributes.DX.value",
@@ -585,7 +616,7 @@ async function handleRoll(event, actor) {
 			t = t.trim();
 			if (!!t)
 				target = parseInt(t);
-			if (isNaN(target)) target = 0;		// Can't roll against a non-integer
+			if (Number.isNaN(target)) target = 0;		// Can't roll against a non-integer
 		}
 	}
 	if ("damage" in element.dataset) {
