2e8f38f025c64b7191bdf33359e0fb8122b02b44
crnormand <crnormand@bellsouth.net>
2021-02-16 12:54:36 -0500

    Fixed 3d6/1d6 roll issue

--- module/gurps.js
+++ module/gurps.js
@@ -587,8 +587,8 @@ async function performAction(action, actor, event, targets) {
   }
 
   if (action.type === 'roll') {
-    prefix = 'Rolling ' + action.displayformula + ' ' + action.desc
-    formula = action.rollformula
+    prefix = 'Rolling ' + ((!!action.displayformula) ? action.displayformula : action.formula) + ' ' + action.desc
+    formula = action.formula
     if (!!action.costs) targetmods.push(GURPS.ModifierBucket.makeModifier(0, action.costs))
   }
 
