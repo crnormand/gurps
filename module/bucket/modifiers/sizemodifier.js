da732ef79d9b824f335e0ecccc5225ada5018af7
Gus <gustavo.ludwig@gmail.com>
2021-03-03 09:37:21 +0000

    reading size modifiers from separate file

---
+++ module/bucket/modifiers/sizemodifier.js
@@ -0,0 +1,28 @@
+'use strict'
+
+export const sizeModifier = [
+    'Size Modifier (melee diff, ranged abs)',
+    '-10  Size 0.05 yard (1.8")',
+    '-9  Size 0.07 yard (2.5")',
+    '-8  Size 0.1 yard (3.5")',
+    '-7  Size 0.15 yard (5")',
+    '-6  Size 0.2 yard (7")',
+    '-5  Size 0.3 yard (10")',
+    '-4  Size 0.5 yard (18")',
+    "-3  Size 0.7 yard (2')",
+    "-2  Size 1 yard (3')",
+    "-1  Size 1.5 yards (4.5')",
+    "+0  Size 2 yards (6')",
+
+    "+1  Size 3 yards (9')",
+    "+2  Size 5 yards (15')",
+    "+3  Size 7 yards (21')",
+    "+4  Size 10 yards (30')",
+    "+5  Size 15 yards (45')",
+    "+6  Size 20 yards (60')",
+    "+7  Size 30 yards (90')",
+    "+8  Size 50 yards (150')",
+    "+9  Size 70 yards (210')",
+    "+10 Size 100 yards (300')",
+    "+11 Size 150 yards (450')",
+]
--- module/modifiers.js
+++ module/modifiers.js
@@ -2,6 +2,7 @@ import { displayMod, makeSelect, horiz } from '../lib/utilities.js'
 import { parselink } from '../lib/parselink.js'
 import * as Settings from '../lib/miscellaneous-settings.js'
 import * as HitLocations from '../module/hitlocation/hitlocation.js'
+import { sizeModifier } from './bucket/modifiers/sizemodifier.js'
 
 Hooks.once('init', async function () {
   Hooks.on('closeModifierBucketEditor', (editor, element) => {
@@ -355,7 +356,7 @@ export class ModifierBucketEditor extends Application {
     data.rofmods = RateOfFireModifiers
     data.statusmods = makeSelect(StatusModifiers)
     data.covermods = makeSelect(CoverPostureModifiers)
-    data.sizemods = SizeModifiers
+    data.sizemods = sizeModifier
     data.hitlocationmods = HitlocationModifiers
     data.currentmods = []
 
@@ -547,33 +548,6 @@ const CoverPostureModifiers = [
   '-2 to hit Ranged (Crouch)',
   '-2 to hit Melee (Kneel/Sit)',
   '-2 to active defenses (Kneel/Sit)',
-]
-
-const SizeModifiers = [
-  'Size Modifier (melee diff, ranged abs)',
-  '-10  Size 0.05 yard (1.8")',
-  '-9  Size 0.07 yard (2.5")',
-  '-8  Size 0.1 yard (3.5")',
-  '-7  Size 0.15 yard (5")',
-  '-6  Size 0.2 yard (7")',
-  '-5  Size 0.3 yard (10")',
-  '-4  Size 0.5 yard (18")',
-  "-3  Size 0.7 yard (2')",
-  "-2  Size 1 yard (3')",
-  "-1  Size 1.5 yards (4.5')",
-  "+0  Size 2 yards (6')",
-
-  "+1  Size 3 yards (9')",
-  "+2  Size 5 yards (15')",
-  "+3  Size 7 yards (21')",
-  "+4  Size 10 yards (30')",
-  "+5  Size 15 yards (45')",
-  "+6  Size 20 yards (60')",
-  "+7  Size 30 yards (90')",
-  "+8  Size 50 yards (150')",
-  "+9  Size 70 yards (210')",
-  "+10 Size 100 yards (300')",
-  "+11 Size 150 yards (450')",
 ]
 
 let HitlocationModifiers = ['Hit Locations (if miss by 1, then *)']
