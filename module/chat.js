58e5d78ea340d2e2d8c27251a2cdc7e3a1d4c64d
crnormand <crnormand@bellsouth.net>
2021-03-10 12:29:38 -0500

    C:/Program Files/Git/status clear and update to /select !

--- module/chat.js
+++ module/chat.js
@@ -104,7 +104,7 @@ export default function addChatHooks() {
           c += '<br>/roll (or /r) [On-the-Fly formula]'
           c += '<br>/select &lt;Actor name&gt'
           c += '<br>/showmbs'
-          c += '<br>/status on|off|t|toggle &lt;status&gt;'
+          c += '<br>/status on|off|t|toggle|clear &lt;status&gt;'
           c += '<br>/tracker(&lt;name&gt;) &lt;formula&gt;'
           c += '<br>/trackerN (N=0-3) &lt;formula&gt;'
           c += '<br>/uses &lt;formula&gt; &lt;equipment name&gt;'
@@ -132,7 +132,7 @@ export default function addChatHooks() {
             priv("Clearing Last Actor", msgs)
           } else {
             let pat = m[2].split('*').join('.*')
-            let list = Object.values(game.scenes.entries).filter(s => s._view)[0].data.tokens.map(t => game.actors.get(t.actorId)) 
+            let list = game.scenes.viewed?.data.tokens.map(t => game.actors.get(t.actorId)) || []
             if (!!m[3]) list = game.actors.entities
             let a = list.filter(a => a.name.match(pat));
             if (a.length == 0) ui.notifications.warn("No Actor found matching '" + m[2] + "'")
@@ -146,13 +146,14 @@ export default function addChatHooks() {
           return              
         }
                 
-        m = line.match(/\/(st|status) +(t|toggle|on|off|\+|-) +([^ ]+) +(\@self)?/i)
+        m = line.match(/\/(st|status) +(t|toggle|on|off|\+|-|clear|set|unset) *([^\@ ]+)? *(\@self)?/i)
         if (!!m) {
-          let pattern =  new RegExp('^' + (m[3].trim().split('*').join('.*').replace(/\(/g, '\\(').replace(/\)/g, '\\)')), 'i') // Make string into a RegEx pattern
+          let pattern = !m[3] ? ".*" : new RegExp('^' + (m[3].trim().split('*').join('.*').replace(/\(/g, '\\(').replace(/\)/g, '\\)')), 'i') // Make string into a RegEx pattern
           let any = false
           let on = false
-          let set = m[2].toLowerCase() == "on" || m[2] == '+'
+          let set = m[2].toLowerCase() == "on" || m[2] == '+' || m[2] == 'set'
           let toggle = m[2].toLowerCase() == "t" || m[2].toLowerCase() == "toggle"
+          let clear = m[2].toLowerCase() == "clear"
           var effect, msg
           Object.values(CONFIG.statusEffects).forEach(s => {
             let nm = game.i18n.localize(s.label)
@@ -169,13 +170,20 @@ export default function addChatHooks() {
               else {
                 any = true
                 GURPS.LastActor.effects.forEach(e => { 
-                  if (effect.id == e.getFlag("core", "statusId")) on = true 
+                  if (clear) Object.values(CONFIG.statusEffects).forEach(s => { 
+                      if (s.id == e.getFlag("core", "statusId")) {
+                        tokens[0].toggleEffect(s)
+                        prnt(`Clearing ${e.data.label} for ${GURPS.LastActor.name}`, msgs)
+                      }
+                  })
+                  else if (effect.id == e.getFlag("core", "statusId")) on = true
+
                 })
                 if ((on & !set) || (!on && set) || toggle) {
                   prnt(`Toggling ${game.i18n.localize(effect.label)} for ${GURPS.LastActor.name}`, msgs)
                   tokens[0].toggleEffect(effect)
                 }
-              } 
+              }
             } else msg = "You must select a character to apply status effects"
           } else {
             msg = "You must select tokens (or use @self) to apply status effects"
@@ -183,7 +191,13 @@ export default function addChatHooks() {
               any = true
               if (!!t.actor) 
                 t.actor.effects.forEach(e => { 
-                  if (effect.id == e.getFlag("core", "statusId")) on = true 
+                  if (clear) Object.values(CONFIG.statusEffects).forEach(s => { 
+                    if (s.id == e.getFlag("core", "statusId")) {
+                      t.toggleEffect(s)
+                      prnt(`Clearing ${e.data.label} for ${t.actor.name}`, msgs)
+                    }
+                  })
+                  else if (effect.id == e.getFlag("core", "statusId")) on = true
                 })
               if ((on & !set) || (!on && set) || toggle) {
                 prnt(`Toggling ${game.i18n.localize(effect.label)} for ${t.actor.name}`, msgs)
