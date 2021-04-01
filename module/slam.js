af971fe234a4778acba104dff6d01f1a49e917c8
Jeff Wilson <geek.macbeer@gmail.com>
2021-04-01 17:41:22 -0400

    initial slam calculator implementation

--- module/slam.js
+++ module/slam.js
@@ -2,8 +2,11 @@
 
 import { ChatProcessors, ChatProcessor } from '../module/chat.js'
 import selectTarget from '../module/select-target.js'
-import { GurpsRoll } from './modifiers.js'
+import { isNiceDiceEnabled } from '../lib/utilities.js'
 
+/**
+ * Handle the '/slam' command. Must have a selected actor. The
+ */
 export default class SlamChatProcessor extends ChatProcessor {
   static initialize() {
     ChatProcessors.registerProcessor(new SlamChatProcessor())
@@ -26,15 +29,15 @@ export default class SlamChatProcessor extends ChatProcessor {
 
     // see if there are any targets
     let targets = actor.getUsers().flatMap(u => [...u.targets])
-    let selected = null
+    let target = null
     if (targets.length > 1) {
       let promise = selectTarget()
       promise.then(target => {
         SlamCalculator.process(actor, target)
       })
     } else {
-      if (targets.length === 1) selected = [...targets][0]
-      SlamCalculator.process(actor, selected)
+      if (targets.length === 1) target = [...targets][0]
+      SlamCalculator.process(actor, target)
     }
 
     return true
@@ -49,20 +52,26 @@ class SlamCalculator extends FormApplication {
 
   /**
    * Construct the dialog.
-   * @param {Actor} actor
+   * @param {Actor} attacker
    * @param {Token} target
    * @param {*} options
    */
-  constructor(actor, target, options = {}) {
+  constructor(attacker, target, options = {}) {
     super(options)
 
-    this._actor = actor
+    // find out the token name for a given actor
+    let activeScene = game.scenes.active
+    let tokens = activeScene.data.tokens
+    let name = tokens.find(it => it.actorId === attacker.id).name
+
+    this._attacker = attacker
+    this._name = !!name ? name : attacker.name
     this._target = target
 
     this._calculator = new _SlamCalculator()
 
-    this._attackerHp = !!actor ? actor.data.data.HP.max : 10
-    this._attackerSpeed = !!actor ? parseInt(actor.data.data.basicmove.value) : 5
+    this._attackerHp = !!attacker ? attacker.data.data.HP.max : 10
+    this._attackerSpeed = !!attacker ? parseInt(attacker.data.data.basicmove.value) : 5
 
     this._targetHp = !!target ? target.actor.data.data.HP.max : 10
     this._targetSpeed = 0
@@ -91,8 +100,9 @@ class SlamCalculator extends FormApplication {
     data.targetHp = this._targetHp
     data.targetSpeed = this._targetSpeed
     data.relativeSpeed = this.relativeSpeed
-    data.attacker = this._actor.data.token.name
-    data.target = !!this._target ? this._target.data.name : 'a target'
+    data.attacker = this._name
+    data.isAoAStrong = this._isAoAStrong
+    data.target = !!this._target ? this._target.data.name : `(${game.i18n.localize('GURPS.target')})`
     return data
   }
 
@@ -102,7 +112,7 @@ class SlamCalculator extends FormApplication {
 
   _updateObject(event, html) {
     this._isAoAStrong = $(event.target).find('#aoa').is(':checked')
-    this._calculator.process(this)
+    this._calculator.process(this.getData(), this._attacker, this._target)
   }
 
   /** @override */
@@ -122,14 +132,54 @@ class SlamCalculator extends FormApplication {
 }
 
 class _SlamCalculator {
-  process(data) {
+  async process(data, attacker, target) {
     // If you hit, you and your foe each inflict dice of
     // crushing damage on the other equal to (HP x velocity)/100.
-    let rawDamageAttacker = (data._attackerHp * data.relativeSpeed) / 100
-    let attackerDice = this._getDicePlusAdds(rawDamageAttacker, data._isAoAStrong)
+    let rawDamageAttacker = (data.attackerHp * data.relativeSpeed) / 100
+    let attackerDice = this._getDicePlusAdds(rawDamageAttacker, data.isAoAStrong)
 
-    let rawDamageTarget = (data._targetHp * data.relativeSpeed) / 100
+    let rawDamageTarget = (data.targetHp * data.relativeSpeed) / 100
     let targetDice = this._getDicePlusAdds(rawDamageTarget)
+
+    let attackerRoll = Roll.create(`${attackerDice.dice}d6! + ${attackerDice.adds}`)
+    attackerRoll.evaluate()
+
+    let targetRoll = Roll.create(`${targetDice.dice}d6! + ${targetDice.adds}`)
+    targetRoll.evaluate()
+
+    let message = `${data.target} ${game.i18n.localize('GURPS.notAffected')}.`
+    if (attackerRoll.total >= targetRoll.total * 2) {
+      message = `${data.target} ${game.i18n.localize('GURPS.fallsDown')}!`
+    } else if (attackerRoll.total >= targetRoll.total) {
+      message = `${data.target}: ${game.i18n.localize('GURPS.roll')} [${game.i18n.localize(
+        'GURPS.attributesDX'
+      )}${game.i18n.localize('GURPS.toAvoidFalling')}]`
+    } else if (targetRoll.total >= attackerRoll.total * 2) {
+      message = `${data.attacker} ${game.i18n.localize('GURPS.fallsDown')}!`
+    }
+
+    let html = await renderTemplate('systems/gurps/templates/slam-results.html', {
+      attacker: data.attacker,
+      target: data.target,
+      isAoAStrong: data.isAoAStrong,
+      result: message,
+    })
+
+    const speaker = { alias: attacker.name, _id: attacker._id, actor: attacker }
+    let messageData = {
+      user: game.user._id,
+      speaker: speaker,
+      content: html,
+      type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+      roll: attackerRoll,
+      sound: this.rollThemBones([targetRoll]),
+    }
+
+    CONFIG.ChatMessage.entityClass.create(messageData).then(arg => {
+      console.log(arg)
+      // let messageId = arg.data._id // 'qHz1QQuzpJiavH3V'
+      // $(`[data-message-id='${messageId}']`).click(ev => game.GURPS.handleOnPdf(ev))
+    })
 
     // Chat message:
     // Slam Attack
@@ -151,20 +201,27 @@ class _SlamCalculator {
     // attackerDice = Roll.create('1d6-1!')
     console.log(
       `Slam Attack
-  Attacker ${data._actor.data.name} slams ${data._target.data.name}.
+  ${data.attacker} slams ${data.target}.
   ---
   Relative Speed: ${data.relativeSpeed}
-  ${data._actor.data.name}:
-    HP (${data._attackerHp}) x Speed (${data.relativeSpeed}) / 100 = ${rawDamageAttacker}
+  ${data.attacker}:
+    HP (${data.attackerHp}) x Speed (${data.relativeSpeed}) / 100 = ${rawDamageAttacker}
     Dice: ${attackerDice.dice}d${attackerDice.adds >= 0 ? '+' : ''}${attackerDice.adds}
 
-  ${data._target.data.name}:
-    HP (${data._targetHp}) x Speed (${data.relativeSpeed}) / 100 = ${rawDamageTarget}
+  ${data.target}:
+    HP (${data.targetHp}) x Speed (${data.relativeSpeed}) / 100 = ${rawDamageTarget}
     Dice: ${targetDice.dice}d${targetDice.adds >= 0 ? '+' : ''}${targetDice.adds}
 `
     )
   }
 
+  /**
+   * Calculate the dice roll from the rawDamage value.
+   *
+   * @param {Number} rawDamage
+   * @param {Boolean} isAoAStrong
+   * @returns an Object literal with two attributes: dice (integer) and adds (integer)
+   */
   _getDicePlusAdds(rawDamage, isAoAStrong = false) {
     let bonusForAoA = isAoAStrong ? 2 : 0
     // If damage is less than 1d, ...
@@ -189,5 +246,37 @@ class _SlamCalculator {
     if (isAoAStrong) adds = dice > 1 ? dice : 2
 
     return { dice: dice, adds: adds }
+  }
+
+  /**
+   *
+   * @param {Aray<Roll>} rollArray
+   */
+  rollThemBones(rollArray) {
+    if (!isNiceDiceEnabled()) return CONFIG.sounds.dice
+
+    if (!Array.isArray(rollArray)) {
+      rollArray = [rollArray]
+    }
+
+    let dice = []
+    rollArray.forEach(r => {
+      r.dice.forEach(d => {
+        let type = 'd' + d.faces
+        d.results.forEach(s =>
+          dice.push({
+            result: s.result,
+            resultLabel: s.result,
+            type: type,
+            vectors: [],
+            options: {},
+          })
+        )
+      })
+    })
+
+    if (dice.length > 0) {
+      game.dice3d.show({ throws: [{ dice: dice }] })
+    }
   }
 }
