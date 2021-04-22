abe4b5422de2c480c6a3974602f31536a7bc02d2
Jeff Wilson <geek.macbeer@gmail.com>
2021-04-22 14:30:10 -0400

    completed the system settings and editor

--- module/modifier-bucket/select-journals.js
+++ module/modifier-bucket/select-journals.js
@@ -4,25 +4,20 @@ import { i18n, arrayToObject, objectToArray } from '../../lib/utilities.js'
 export default class ModifierBucketJournals extends FormApplication {
   static getAllJournals() {
     let journals = game.settings.get(settings.SYSTEM_NAME, settings.SETTING_BUCKET_JOURNALS)
-    // verify each journal (still) exists, removing them if they do not
-
-    return journals
+    return objectToArray(journals)
   }
 
   constructor(options = {}) {
     super(options)
-
-    this._journals = ['WoignqCRLfn5zuWg']
-    //    ModifierBucketJournals.getAllJournals()
   }
 
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       id: 'modifier-journals',
       template: 'systems/gurps/templates/modifier-bucket/select-journals.html',
-      resizeable: false,
+      resizeable: true,
       minimizable: false,
-      width: 400,
+      width: 550,
       height: 'auto',
       title: i18n('GURPS.bucketJournalManager', 'Modifier Bucket Journals'),
       closeOnSubmit: true,
@@ -34,19 +29,23 @@ export default class ModifierBucketJournals extends FormApplication {
    */
   getData(options) {
     const data = super.getData(options)
-    data.journals = this._journals
+    data.journals = ModifierBucketJournals.getAllJournals()
     data.allJournals = this._htmlJournals
     return data
   }
 
   /**
-   * @override
-   * @param {HtmlElement} html - JQuery reference to this form element
+   * @returns {Array} an array of all journals which have text in the data.content field.
+   * Each entry contains the journal's entity ID, folder hierarchy, and name. This list
+   * is sorted by folder hierarchy + name.
+   *
+   * Entry:
+   * {
+   *   name: String,
+   *   folder: String,
+   *   id: String
+   * }
    */
-  activateListeners(html) {
-    super.activateListeners(html)
-  }
-
   get _htmlJournals() {
     let allJournals = Array.from(game.journal)
     let htmlJournals = allJournals.filter(it => !!it.data.content)
@@ -56,17 +55,32 @@ export default class ModifierBucketJournals extends FormApplication {
     return results.sort((a, b) => `${a.folder}/${a.name}`.localeCompare(`${b.folder}/${b.name}`))
   }
 
+  /**
+   * @param {String} id of the journal entry
+   * @param {String} name the String that contains the working folder path; only used by the
+   * function internals. Should not be passed in the initial invocation.
+   *
+   * @returns a String that shows the folder hierarchy/path to where the journal entry exists
+   */
   _folderPath(id, name = '') {
     let folder = game.folders.get(id)
-    if (!!folder.parent) {
+
+    if (!!folder && !!folder.parent) {
       name = this._folderPath(folder.parent.id, name) + '/'
     }
-    name = name + folder.name
+    if (!!folder) name = name + folder.name
     return name
   }
 
   /**
    * @override
    */
-  _updateObject() {}
+  _updateObject(event, formData) {
+    let html = $(event.currentTarget)
+    let checkboxes = html.find('#entry-list').find('input[type="checkbox"]:checked')
+    let ids = Array.from(checkboxes).map(it => it.id)
+
+    let data = arrayToObject(ids)
+    game.settings.set(settings.SYSTEM_NAME, settings.SETTING_BUCKET_JOURNALS, data)
+  }
 }
