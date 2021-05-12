11377aae7e5de4238f1e0889421ffcbe6147af9d
crnormand <crnormand@bellsouth.net>
2021-05-11 23:00:27 -0400

    Part 3 Items, bug fixes

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -31,14 +31,17 @@ export class GurpsItemSheet extends ItemSheet {
     this.html = html
     super.activateListeners(html);
     
-    html.find('.name').change(ev => 
+    html.find('#itemname').change(ev => 
       this.item.update({ "data.eqt.name": ev.currentTarget.value }))
     html.find('.count').change(ev => 
       this.item.update({ "data.eqt.count": parseInt(ev.currentTarget.value) }))
 
     html.find('#item1').click(ev => {
       ev.preventDefault()
-      let m = new Melee('Test Melee Weapon 1', 14, '2d cut')
+      let m = new Melee()
+      m.name  = this.item.name
+      m.level = 14
+      m.damage = '2d cut'
       m.mode = 'swing'
       m.otf = 'DX-1'
       m.weight = 99
@@ -47,14 +50,14 @@ export class GurpsItemSheet extends ItemSheet {
       m.reach = '99'
       m.parry = '9'
       m.block = '8'
-      let melee = this.object.data.data.melee
+      let melee = this.item.data.data.melee
       GURPS.put(melee, m)
       this.item.update({ "data.melee" : melee })
     })
     html.find('#item2').click(ev => {
       ev.preventDefault()
       let r = new Ranged()
-      r.name = "Test ranged"
+      r.name = this.item.name
       r.otf = 'DX-2'
       r.type = 'DX/E'
       r.bulk = 1
@@ -67,18 +70,18 @@ export class GurpsItemSheet extends ItemSheet {
       r.rof = 1
       r.shots = ""
       r.rcl = ""
-      let list = this.object.data.data.ranged
+      let list = this.item.data.data.ranged
       GURPS.put(list, r)
       this.item.update({ "data.ranged" : list })
     })
     html.find('#item3').click(ev => {
       ev.preventDefault()
       let r = new Skill()
-      r.name = "Test skill"
+      r.name = "Skill for " + this.item.name
       r.otf = 'IQ-4|Traps-2'
       r.level = 11
       r.relativelevel = "IQ-4"
-      let list = this.object.data.data.skills
+      let list = this.item.data.data.skills
       GURPS.put(list, r)
       this.item.update({ "data.skills" : list })
     })
