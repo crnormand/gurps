bf6669c1d77438d809cfb4714e0f2f7820481a14
Jeff Wilson <geek.macbeer@gmail.com>
2021-10-19 09:04:34 -0400

    add Combat Move to Conditions

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -166,13 +166,11 @@ export class GurpsActor extends Actor {
     let v = /** @type {SemanticVersion} */ (SemanticVersion.fromString(data.migrationversion))
 
     // Attributes need to have 'value' set because Foundry expects objs with value and max to be attributes (so we can't use currentvalue)
-		// Need to protect against data errors
+    // Need to protect against data errors
     for (const attr in data.attributes) {
-			if (typeof data.attributes[attr] === 'object' && data.attributes[attr] !== null)
-			  if (isNaN(data.attributes[attr].import))
-		      data.attributes[attr].value = 0
-			  else
-					data.attributes[attr].value = parseInt(data.attributes[attr].import)
+      if (typeof data.attributes[attr] === 'object' && data.attributes[attr] !== null)
+        if (isNaN(data.attributes[attr].import)) data.attributes[attr].value = 0
+        else data.attributes[attr].value = parseInt(data.attributes[attr].import)
     }
     // After all of the attributes are copied over, apply tired to ST
     if (!!data.additionalresources.isTired)
@@ -403,6 +401,7 @@ export class GurpsActor extends Actor {
     const encs = data.encumbrance
     const isReeling = !!data.additionalresources.isReeling
     const isTired = !!data.additionalresources.isTired
+
     // We must assume that the first level of encumbrance has the finally calculated move and dodge settings
     if (!!encs) {
       const level0 = encs[GURPS.genkey(0)] // if there are encumbrances, there will always be a level0
@@ -462,7 +461,7 @@ export class GurpsActor extends Actor {
           return this._getStep()
         case MOVE_HALF:
           this.getGurpsActorData().conditions.move = i18n('GURPS.moveHalf')
-          move = Math.ceil(move / 2)
+          return Math.ceil(move / 2)
         case MOVE_FULL:
           this.getGurpsActorData().conditions.move = i18n('GURPS.moveFull')
           break
