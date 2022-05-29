39d1ac38782a67b380021ac98ac3212719146837
mjeffw <geek.macbeer@gmail.com>
2022-05-29 13:41:07 -0400

    move gurpslink() function to its own file

--- module/gurps.js
+++ module/gurps.js
@@ -1378,37 +1378,37 @@ GURPS.applyModifierDesc = applyModifierDesc
  * @param {string | null | undefined} str
  * @param {boolean} [clrdmods=true]
  */
-function gurpslink(str, clrdmods = true, returnActions = false) {
-  if (str === undefined || str == null) return '!!UNDEFINED'
-  let found = -1
-  let depth = 0
-  let output = ''
-  let actions = []
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == '[') {
-      if (depth == 0) found = ++i
-      depth++
-    }
-    if (str[i] == ']') {
-      depth--
-      if (depth == 0 && found >= 0) {
-        output += str.substring(0, found - 1)
-        let action = parselink(str.substring(found, i), '', clrdmods)
-        if (!!action.action) actions.push(action)
-        if (!action.action) output += '['
-        output += action.text
-        if (!action.action) output += ']'
-        str = str.substr(i + 1)
-        i = -1
-        found = -1
-      }
-    }
-  }
-  if (returnActions === true) return actions
-  output += str
-  return output
-}
-GURPS.gurpslink = gurpslink
+// function gurpslink(str, clrdmods = true, returnActions = false) {
+//   if (str === undefined || str == null) return '!!UNDEFINED'
+//   let found = -1
+//   let depth = 0
+//   let output = ''
+//   let actions = []
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == '[') {
+//       if (depth == 0) found = ++i
+//       depth++
+//     }
+//     if (str[i] == ']') {
+//       depth--
+//       if (depth == 0 && found >= 0) {
+//         output += str.substring(0, found - 1)
+//         let action = parselink(str.substring(found, i), '', clrdmods)
+//         if (!!action.action) actions.push(action)
+//         if (!action.action) output += '['
+//         output += action.text
+//         if (!action.action) output += ']'
+//         str = str.substr(i + 1)
+//         i = -1
+//         found = -1
+//       }
+//     }
+//   }
+//   if (returnActions === true) return actions
+//   output += str
+//   return output
+// }
+//GURPS.gurpslink = gurpslink
 
 /**
  * Return the i18n string for this data path (note en.json must match up to the data paths).
