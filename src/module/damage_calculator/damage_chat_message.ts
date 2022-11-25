af2aa4849701b4a521f01c7b6c2ece02547423d5
mjeffw <geek.macbeer@gmail.com>
2022-11-25 13:58:43 -0500

    wip commit

--- src/module/damage_calculator/damage_chat_message.ts
+++ src/module/damage_calculator/damage_chat_message.ts
@@ -1,13 +1,32 @@
 export default class DamageChat {
+	static TYPE = "damage"
+
 	static async renderChatMessage(app: ChatMessage, html: JQuery<HTMLElement>, msg: any) {
 		if (!html.find(".message-roll.damage").length) return true
 
 		// Let transfer = JSON.parse(app?.flags.transfer)
+		let payload = {}
 
 		const dragSections = html.find(".result")
 		for (const section of dragSections) {
 			// Section.setAttribute("draggable", "true")
+			section.addEventListener("dragstart", DamageChat._dragStart.bind(this, payload))
+			section.addEventListener("dragend", DamageChat._dragEnd)
 		}
 		return false
+	}
+
+	static async _dragStart(payload: any, ev: DragEvent) {
+		return ev.dataTransfer?.setData(
+			"text/plain",
+			JSON.stringify({
+				type: DamageChat.TYPE,
+				payload: payload,
+			})
+		)
+	}
+
+	static async _dragEnd(ev: DragEvent) {
+		// Add any handling code here.
 	}
 }
