10571a86a89aeca206142f9e6fd9cd761f5aa8b1
mjeffw <geek.macbeer@gmail.com>
2022-11-25 13:14:06 -0500

    make damage chat section draggable

---
+++ src/module/damage_calculator/damage_chat_message.ts
@@ -0,0 +1,13 @@
+export default class DamageChat {
+	static async renderChatMessage(app: ChatMessage, html: JQuery<HTMLElement>, msg: any) {
+		if (!html.find(".message-roll.damage").length) return true
+
+		// Let transfer = JSON.parse(app?.flags.transfer)
+
+		const dragSections = html.find(".result")
+		for (const section of dragSections) {
+			section.setAttribute("draggable", true)
+		}
+		return false
+	}
+}
