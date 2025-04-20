bb30c308dd2118897fdab0b4ef766e3b065d7c68
mjeffw <geek.macbeer@gmail.com>
2025-04-20 12:14:48 -0400

    refactor json-key-diff and json-key-diff-repair to improve key walking logic and enhance file comparison functionality

---
+++ dev-utilities/json-key-diff-repair.js
@@ -0,0 +1,90 @@
+#! /usr/local/bin/node
+//const fs = require('fs')
+import fs from 'fs'
+
+let args = process.argv.slice(2)
+
+const targetFile = enFile
+const enFile = './lang/en.json'
+
+const file1 = fs.readFileSync(targetFile, 'utf8')
+
+let en_json = JSON.parse(fs.readFileSync(enFile, 'utf8'))
+let object2 = JSON.parse(fs.readFileSync(targetFile, 'utf8'))
+
+// Recursively walk the keys in object1 and collect the fully qualified keys in a variable
+function walk(obj, prefix, keys) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      walk(obj[key], prefix + key + '.', keys)
+    } else {
+      keys.push(prefix + key)
+    }
+  }
+}
+
+let en_keys = []
+let keys2 = []
+walk(en_json, '', en_keys)
+walk(object2, '', keys2)
+
+console.log('------------')
+console.log(`keys in [${targetFile}] that are missing in [${enFile}]`)
+let missingInFile1 = keys2.filter(key => !en_keys.includes(key)).sort()
+missingInFile1.forEach(it => console.log(`  ${it}`))
+
+// Remove missiingInFile1 entries from object2.
+missingInFile1.forEach(it => {
+  let parts = it.split('.')
+  let obj = object2
+  for (let i = 0; i < parts.length - 1; i++) {
+    obj = obj[parts[i]]
+  }
+  delete obj[parts[parts.length - 1]]
+})
+// overwrite targetFile
+fs.writeFileSync(targetFile, JSON.stringify(object2, null, 2))
+
+console.log('------------')
+console.log(`keys in [${enFile}] that are missing in [${targetFile}]`)
+let missingInFile2 = en_keys.filter(key => !keys2.includes(key)).sort()
+missingInFile2.forEach(it => console.log(`  ${it}`))
+// Add missingInFile2 entries to object2.
+missingInFile2.forEach(it => {
+  let parts = it.split('.')
+  let obj = en_json
+  for (let i = 0; i < parts.length - 1; i++) {
+    obj = obj[parts[i]]
+  }
+  let value = obj[parts[parts.length - 1]]
+  let obj2 = object2
+  for (let i = 0; i < parts.length - 1; i++) {
+    if (!obj2[parts[i]]) {
+      obj2[parts[i]] = {}
+    }
+    obj2 = obj2[parts[i]]
+  }
+  obj2[parts[parts.length - 1]] = value
+})
+// Overwrite targetFile.
+fs.writeFileSync(targetFile, JSON.stringify(object2, null, 2))
+
+console.log('------------')
+console.log(`values in [${enFile}] that are the same in [${targetFile}]`)
+let sameKeys = Object.entries(en_json)
+  .filter(([k, _]) => keys2.includes(k))
+  .filter(([k, v]) => object2[k] === v)
+  .sort()
+sameKeys.forEach(([k, v]) => console.log(`  ${k}: ${v}`))
+// Remove sameKeys entries from object2.
+sameKeys.forEach(([k, v]) => {
+  let parts = k.split('.')
+  let obj = object2
+  for (let i = 0; i < parts.length - 1; i++) {
+    obj = obj[parts[i]]
+  }
+  delete obj[parts[parts.length - 1]]
+})
+// Overwrite targetFile
+fs.writeFileSync(targetFile, JSON.stringify(object2, null, 2))
+console.log('------------')
--- dev-utilities/json-key-diff.js
+++ dev-utilities/json-key-diff.js
@@ -4,11 +4,26 @@ import fs from 'fs'
 
 let args = process.argv.slice(2)
 
+const file1 = fs.readFileSync(args[0], 'utf8')
+
 let object1 = JSON.parse(fs.readFileSync(args[0], 'utf8'))
 let object2 = JSON.parse(fs.readFileSync(args[1], 'utf8'))
 
-let keys1 = Object.keys(object1)
-let keys2 = Object.keys(object2)
+// Recursively walk the keys in object1 and collect the fully qualified keys in a variable
+function walk(obj, prefix, keys) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      walk(obj[key], prefix + key + '.', keys)
+    } else {
+      keys.push(prefix + key)
+    }
+  }
+}
+
+let keys1 = []
+let keys2 = []
+walk(object1, '', keys1)
+walk(object2, '', keys2)
 
 console.log('------------')
 console.log(`keys in [${args[1]}] that are missing in [${args[0]}]`)
@@ -27,3 +42,5 @@ let sameKeys = Object.entries(object1)
   .filter(([k, v]) => object2[k] === v)
   .sort()
 sameKeys.forEach(([k, v]) => console.log(`  ${k}: ${v}`))
+console.log('------------')
+
