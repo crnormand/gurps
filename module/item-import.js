6a081b21d8b127cf79d82fda1ddd7e3d64c7f914
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-05-15 21:39:17 +0100

    removed unnecessary space in text node

--- module/item-import.js
+++ module/item-import.js
@@ -42,7 +42,7 @@ export const AddImportEquipmentButton = async function (html) {
   const icon = document.createElement('i')
   icon.classList.add('fa-solid', 'fa-file-import')
   button.appendChild(icon)
-  const textNode = document.createTextNode(' ' + game.i18n.localize('GURPS.itemImport'))
+  const textNode = document.createTextNode(game.i18n.localize('GURPS.itemImport'))
   button.appendChild(textNode)
 
   if (game.release.generation === 12) {
