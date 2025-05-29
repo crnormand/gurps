c91f469f9a562dc646e01c93147f45f886a813be
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-05-29 14:38:05 +0100

    Started on process of converting items and actor components to DataModels

---
+++ module/item/data/trait.ts
@@ -0,0 +1,53 @@
+import { CommonItemData, CommonItemDataSchema, ItemComponent, ItemComponentSchema } from './base.js'
+import fields = foundry.data.fields
+
+class TraitData extends CommonItemData<TraitSchema> {
+  static override defineSchema(): TraitSchema {
+    return {
+      ...super.defineSchema(),
+      ...traitSchema,
+    }
+  }
+
+  /* ---------------------------------------- */
+
+  get component(): TraitComponent {
+    return this.fea
+  }
+}
+
+/* ---------------------------------------- */
+
+class TraitComponent extends ItemComponent<TraitComponentSchema> {
+  static override defineSchema(): TraitComponentSchema {
+    return {
+      ...super.defineSchema(),
+      ...traitComponentSchema,
+    }
+  }
+}
+
+/* ---------------------------------------- */
+
+const traitSchema = {
+  fea: new fields.EmbeddedDataField(TraitComponent, { required: true, nullable: false }),
+}
+
+type TraitSchema = CommonItemDataSchema & typeof traitSchema
+
+/* ---------------------------------------- */
+
+const traitComponentSchema = {
+  level: new fields.NumberField({ required: true, nullable: false }),
+  userdesc: new fields.StringField({ required: true, nullable: false }),
+  note: new fields.StringField({ required: true, nullable: false }),
+  points: new fields.NumberField({ required: true, nullable: false }),
+  save: new fields.BooleanField({ required: true, nullable: false }),
+  itemid: new fields.StringField({ required: true, nullable: false }),
+}
+
+type TraitComponentSchema = ItemComponentSchema & typeof traitComponentSchema
+
+/* ---------------------------------------- */
+
+export { TraitData, type TraitSchema }
