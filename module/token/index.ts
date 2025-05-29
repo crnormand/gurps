8bb8457ddd6429f9bf654807357593a48d6ed22f
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-05-29 07:53:22 +0100

    Added native movement cost indicators in Foundry v13

--- module/token/index.ts
+++ module/token/index.ts
@@ -1,6 +1,7 @@
 import { GurpsToken } from './gurps-token.js'
 import { GurpsTokenHUD } from './token-hud-12.js'
 import { GurpsTokenHudV2 } from './token-hud.js'
+import { registerTokenRuler } from './token-ruler.js'
 
 export * from './gurps-token.js'
 export * from './quick-roll-settings.js'
@@ -12,6 +13,8 @@ export function init(): void {
     if (!game.release) return
     if (game.release?.generation >= 13) {
       CONFIG.Token.hudClass = GurpsTokenHudV2
+      // COMPATIBILITY: v12
+      registerTokenRuler()
     } else {
       CONFIG.Token.hudClass = GurpsTokenHUD
     }
