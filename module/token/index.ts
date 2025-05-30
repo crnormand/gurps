9a1788009737b74bcf071b04669a23dc1b693cb5
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-05-30 07:40:37 +0100

    Added move limit indicators to token drag ruler in v13

--- module/token/index.ts
+++ module/token/index.ts
@@ -1,6 +1,7 @@
 import { GurpsTokenHUD } from './token-hud-12.js'
 import { GurpsToken } from './gurps-token.js'
 import { registerTokenHUD } from './token-hud.js'
+import { registerTokenRuler } from './token-ruler.js'
 
 export * from './gurps-token.js'
 export * from './quick-roll-settings.js'
@@ -8,11 +9,11 @@ export * from './quick-roll-settings.js'
 export function init(): void {
   Hooks.once('init', () => {
     CONFIG.Token.objectClass = GurpsToken
-
     if (!game.release) return
     if (game.release?.generation >= 13) {
       // COMPATIBILITY: v12
       registerTokenHUD()
+      registerTokenRuler()
     } else {
       CONFIG.Token.hudClass = GurpsTokenHUD
     }
---
+++ module/token/token-ruler.ts
@@ -0,0 +1,52 @@
+// COMPATIBILITY: v12
+function registerTokenRuler() {
+  if (!game.release) return
+  if (game.release?.generation < 13) return
+
+  // @ts-expect-error: waiting for types to catch up
+  class GurpsTokenRuler extends foundry.canvas.placeables.tokens.TokenRuler {
+    /**
+     * Get the style to be used to highlight the grid offset.
+     * @param {DeepReadonly<Omit<TokenRulerWaypoint, "index"|"center"|"size"|"ray">>} waypoint    The waypoint
+     * @param {DeepReadonly<GridOffset3D>} offset  An occupied grid offset at the given waypoint that is to be highlighted
+     * @returns {{color?: PIXI.ColorSource; alpha?: number; texture?: PIXI.Texture; matrix?: PIXI.Matrix | null}}
+     *   The color, alpha, texture, and texture matrix to be used to draw the grid space.
+     *   If the alpha is 0, the grid space is not highlighted.
+     */
+    // @ts-expect-error: waiting for types to catch up
+    protected override _getGridHighlightStyle(
+      // @ts-expect-error: waiting for types to catch up
+      waypoint: TokenRulerWaypoint,
+      offset: { i: number; j: number }
+    ): { color?: PIXI.ColorSource; alpha?: number; texture?: PIXI.Texture; matrix?: PIXI.Matrix | null } {
+      const data = super._getGridHighlightStyle(waypoint, offset)
+      const actor = this.token.actor
+      if (!actor) return data
+
+      // @ts-expect-error: waiting for actor update to DataModel
+      if (waypoint.measurement.cost <= Math.ceil(actor.system.currentmove / 10)) {
+        return { ...data, color: 0x0000ff }
+        // @ts-expect-error: waiting for actor update to DataModel
+      } else if (waypoint.measurement.cost > actor.system.currentsprint) {
+        return { ...data, color: 0xff0000 }
+        // @ts-expect-error: waiting for actor update to DataModel
+      } else if (waypoint.measurement.cost > actor.system.currentmove) {
+        return { ...data, color: 0xffff00 }
+      } else {
+        return { ...data, color: 0x00ff00 }
+      }
+
+      return data
+    }
+  }
+
+  // COMPATIBILITY: v12
+  interface GurpsTokenRuler {
+    token: Token.Implementation
+  }
+
+  // @ts-expect-error: waiting for types to catch up
+  CONFIG.Token.rulerClass = GurpsTokenRuler
+}
+
+export { registerTokenRuler }
