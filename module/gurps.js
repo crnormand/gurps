518c58060fb45abbc54d7fccc12391e32ede6758
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-05-30 08:01:47 +0100

    Implemented custom Token HUD with included maneuver button, removed dedicated maneuver button class

--- module/gurps.js
+++ module/gurps.js
@@ -27,7 +27,6 @@ import {
   GurpsInventorySheet,
 } from './actor/actor-sheet.js'
 import { GurpsActor } from './actor/actor.js'
-import ManeuverHUDButton from './actor/maneuver-button.js'
 import { ResourceTrackerManager } from './actor/resource-tracker-manager.js'
 import RegisterChatProcessors from './chat/chat-processors.js'
 import { addBucketToDamage, doRoll } from './dierolls/dieroll.js'
@@ -911,9 +910,10 @@ if (!globalThis.GURPS) {
         blind: action.blindroll,
         event,
         obj: att, // save the attack in the optional parameters, in case it has rcl/rof
-        followon: followon,
+        followon,
         text: '',
       }
+      if ('itemPath' in action) opt.itemPath = action.itemPath
       let targetmods = []
       if (opt.obj.checkotf && !(await GURPS.executeOTF(opt.obj.checkotf, false, event, actor))) return false
       if (opt.obj.duringotf) await GURPS.executeOTF(opt.obj.duringotf, false, event, actor)
@@ -1454,7 +1454,7 @@ if (!globalThis.GURPS) {
       opt.itemPath = `@${path}`
 
       const item = foundry.utils.getProperty(actor, path)
-      if ('level' in item) target = item.level
+      if (item && 'level' in item) target = item.level
       opt.obj = item
 
       let srcid = !!actor ? '@' + actor.id + '@' : ''
@@ -1463,6 +1463,8 @@ if (!globalThis.GURPS) {
         const parsedLink = GURPS.parselink(element.dataset.otf)
         if ('action' in parsedLink) action = parsedLink.action
         chatthing = '[' + srcid + element.dataset.otf + ']'
+        action.itemPath = opt.itemPath
+        return performAction(action, actor, event, options?.targets)
       }
     } else if ('path' in element.dataset) {
       let srcid = !!actor ? '@' + actor.id + '@' : ''
@@ -2097,9 +2099,6 @@ if (!globalThis.GURPS) {
       await entity.update({ img: 'systems/gurps/icons/single-die.webp' })
       entity.img = 'systems/gurps/icons/single-die.webp'
     })
-
-    // @ts-ignore
-    Hooks.on('renderTokenHUD', (...args) => ManeuverHUDButton.prepTokenHUD(...args))
 
     Hooks.on('renderActorDirectory', (app, html, context) => {
       // Add the Import Multiple Actors button to the Actors tab.
---
+++ module/token/token-hud.ts
@@ -0,0 +1,70 @@
+import { DeepPartial } from 'fvtt-types/utils'
+
+// COMPATIBILITY: v12
+function registerTokenHUD(): void {
+  if (!game.release) return
+  if (game.release?.generation < 13) return
+
+  class GurpsTokenHUDV2 extends foundry.applications.hud.TokenHUD {
+    static override DEFAULT_OPTIONS: DeepPartial<foundry.applications.api.ApplicationV2.Configuration> = {
+      actions: {
+        maneuver: GurpsTokenHUDV2.#onSetManeuver,
+      },
+    }
+
+    /* ---------------------------------------- */
+
+    static override PARTS: Record<string, foundry.applications.api.HandlebarsApplicationMixin.HandlebarsTemplatePart> =
+      {
+        hud: {
+          root: true,
+          template: 'systems/gurps/templates/hud/token-hud.hbs',
+        },
+      }
+
+    /* ---------------------------------------- */
+
+    protected override async _prepareContext(
+      options: DeepPartial<foundry.applications.api.ApplicationV2.RenderOptions> & { isFirstRender: boolean }
+    ): Promise<foundry.applications.hud.BasePlaceableHUD.RenderContext> {
+      const context = await super._prepareContext(options)
+
+      const activeEffects = this.object.actor?.effects.contents ?? []
+
+      // @ts-expect-error: Waiting for DataModel migration for actor
+      const currentManeuverId = this.object.actor?.system.conditions.maneuver
+      const maneuverIcon =
+        GURPS.Maneuvers.get(currentManeuverId)?.icon ?? 'systems/gurps/icons/maneuvers/man-nothing.png'
+
+      return foundry.utils.mergeObject(context, {
+        icons: { maneuvers: maneuverIcon },
+        // TODO: revise any to specific type
+        maneuvers: Object.entries(GURPS.Maneuvers.getAll()).map(([id, maneuver]: [string, any]) => {
+          return {
+            cssClass: activeEffects.some(effect => effect.icon === maneuver._data.icon) ? 'active' : '',
+            src: maneuver._data.icon,
+            title: game.i18n?.localize(maneuver._data.label) ?? maneuver._data.label,
+            id,
+          }
+        }),
+      })
+    }
+
+    /* ---------------------------------------- */
+
+    static async #onSetManeuver(this: GurpsTokenHUDV2, _event: PointerEvent, target: HTMLElement): Promise<void> {
+      // @ts-expect-error: waiting for types to catch up
+      if (!this.actor) {
+        ui.notifications?.warn('HUD.WarningEffectNoActor', { localize: true })
+        return
+      }
+
+      const maneuverId = target.dataset.statusId || 'do_nothing'
+      this.object.setManeuver(maneuverId)
+    }
+  }
+
+  // @ts-expect-error: waiting for types to catch up
+  CONFIG.Token.hudClass = GurpsTokenHUDV2
+}
+export { registerTokenHUD }
