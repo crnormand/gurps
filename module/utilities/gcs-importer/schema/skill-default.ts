c8767116a534df72ead55620f648981f16db9da4
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-06-09 22:00:46 +0100

    finished GCS schemas

---
+++ module/utilities/gcs-importer/schema/skill-default.ts
@@ -0,0 +1,32 @@
+import { GcsElement } from './base.js'
+import fields = foundry.data.fields
+
+/* ---------------------------------------- */
+
+class GcsSkillDefault extends GcsElement<SkillDefaultData> {
+  static override defineSchema(): SkillDefaultData {
+    return skillDefaultData()
+  }
+}
+
+/* ---------------------------------------- */
+
+const skillDefaultData = () => {
+  return {
+    type: new fields.StringField({ required: true, nullable: false }),
+    name: new fields.StringField({ required: true, nullable: true }),
+    specialization: new fields.StringField({ required: true, nullable: true }),
+    modifier: new fields.NumberField({ required: true, nullable: true }),
+    level: new fields.NumberField({ required: true, nullable: false }),
+    adjusted_level: new fields.NumberField({ required: true, nullable: false }),
+    points: new fields.NumberField({ required: true, nullable: false }),
+    // STUB: when_tl is not yet supported
+    when_tl: new fields.ObjectField({ required: true, nullable: true }),
+  }
+}
+
+type SkillDefaultData = ReturnType<typeof skillDefaultData>
+
+/* ---------------------------------------- */
+
+export { GcsSkillDefault, skillDefaultData, type SkillDefaultData }
