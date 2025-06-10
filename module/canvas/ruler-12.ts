5250a99680c0b2f3bb32c69f96b1e1d54d8d7d03
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-06-10 01:07:05 +0100

    Fixed ruler labels always converting to yards in v12

--- module/canvas/ruler-12.ts
+++ module/canvas/ruler-12.ts
@@ -5,12 +5,13 @@ import { Length, LengthUnit } from '../data/common/index.js'
 export class GurpsRulerV12 extends Ruler {
   override _getSegmentLabel(segment: Ruler.PartialSegmentForLabelling) {
     const totalDistance = this.totalDistance
-    const units = canvas.scene?.grid.units ?? Length.Unit.Yard
+    const units = canvas?.scene?.grid.units ?? Length.Unit.Yard
     let dist = (d: number, u: string) => {
       return `${Math.round(d * 100) / 100} ${u}`
     }
-    const yards = Length.from(totalDistance, units as unknown as LengthUnit, true)?.to(Length.Unit.Yard)
-    let label = yards.toString()
+    const length = Length.from(totalDistance, units as unknown as LengthUnit, true)
+    const yards = length.to(Length.Unit.Yard)
+    let label = length.toString()
     let mod = this.yardsToRangePenalty(yards.value)
     GURPS.ModifierBucket.setTempRangeMod(mod)
     if (segment.last) {
@@ -45,35 +46,4 @@ export class GurpsRulerV12 extends Ruler {
     }
     return 0
   }
-
-  convert_to_yards(numeric: number, Unit: string): number {
-    let meter = 0
-    let unit = Unit.toLowerCase()
-
-    if (meters.includes(unit)) meter = numeric
-    else if (millimeters.includes(unit)) meter = numeric / 1000
-    else if (kilometers.includes(unit)) meter = numeric * 1000
-    else if (miles.includes(unit)) meter = numeric / 0.00062137
-    else if (inches.includes(unit)) meter = numeric / 39.37
-    else if (centimeters.includes(unit)) meter = numeric / 100
-    else if (feet.includes(unit)) meter = numeric / 3.2808
-    else if (yards.includes(unit)) meter = numeric / (3.2808 / 3)
-    else if (lightyears.includes(unit)) meter = numeric * 9460730472580800
-    else if (astronomicalUnits.includes(unit)) meter = numeric * 149597870700
-    else if (parsecs.includes(unit)) meter = numeric * 30856776376340068
-
-    return meter * 1.0936
-  }
 }
-
-const meters = ['meters', 'meter', 'm']
-const millimeters = ['millimeters', 'millimeter', 'mm']
-const kilometers = ['kilometers', 'kilometer', 'km']
-const miles = ['miles', 'mile', 'mi']
-const inches = ['inches', 'inch', 'in']
-const centimeters = ['centimeters', 'centimeter', 'cm']
-const feet = ['feet', 'foot', 'ft']
-const yards = ['yards', 'yard', 'yd', 'y']
-const lightyears = ['lightyears', 'lightyear', 'ly']
-const astronomicalUnits = ['astronomical units', 'astronomical unit', 'au']
-const parsecs = ['parsecs', 'parsec', 'pc']
