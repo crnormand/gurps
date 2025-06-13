bc4d66051deff632e5bb9d2d462ccbd7b7d2661e
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-06-13 14:34:14 +0100

    Started on new character data model

--- module/actor/gurps-actor.ts
+++ module/actor/gurps-actor.ts
@@ -1,3 +1,29 @@
-class GurpsActorV2<SubType extends Actor.SubType> extends Actor<SubType> {}
+class GurpsActorV2<SubType extends Actor.SubType> extends Actor<SubType> {
+  /* ---------------------------------------- */
+
+  isOfType<SubType extends Actor.SubType>(...types: SubType[]): this is ConfiguredActor<SubType>
+  isOfType(...types: string[]): boolean {
+    return types.includes(this.type as Actor.SubType)
+  }
+
+  /* ---------------------------------------- */
+
+  // NOTE: changed from getOnwers() in old system
+  get owners(): User.Implementation[] {
+    return game.users?.filter(user => user.isOwner) ?? []
+  }
+
+  /* ---------------------------------------- */
+
+  // NOTE: STUB. Not convinced this is needed in the new system.
+  //
+  async openSheet(_newSheet: foundry.applications.api.ApplicationV2): Promise<void> {}
+
+  /* ---------------------------------------- */
+
+  /* ---------------------------------------- */
+  /*  Data Preparation                        */
+  /* ---------------------------------------- */
+}
 
 export { GurpsActorV2 }
