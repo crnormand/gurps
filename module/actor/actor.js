9053bef33fdc3c87c998f4a92c131cf1b024f9f1
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-06-14 19:52:40 +0100

    Started transferring over data preparation functions

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -95,6 +95,7 @@ export class GurpsActor extends Actor {
     // this.prepareDerivedData()
   }
 
+  // NOTE: migrated
   prepareBaseData() {
     super.prepareBaseData()
 
@@ -132,6 +133,7 @@ export class GurpsActor extends Actor {
   prepareDerivedData() {
     super.prepareDerivedData()
 
+    // NOTE: migrated as #prepareEncumbrance
     // Handle new move data -- if data.move exists, use the default value in that object to set the move
     // value in the first entry of the encumbrance object.
     if (this.system.encumbrance) {
@@ -261,14 +263,19 @@ export class GurpsActor extends Actor {
   calculateDerivedValues() {
     let saved = !!this.ignoreRender
     this.ignoreRender = true
+    // NOTE: migrated
     this._initializeStartingValues()
+    // NOTE: migrated
     this._applyItemBonuses()
 
     // Must be done after bonuses, but before weights
+    // NOTE: migrated
     this._calculateEncumbranceIssues()
 
     // Must be after bonuses and encumbrance effects on ST
+    // NOTE: not migrated, I'm not convinced this is ever actually needed
     this._recalcItemFeatures()
+    // NOTE: migrated
     this._calculateRangedRanges()
 
     // Must be done at end
@@ -633,6 +640,7 @@ export class GurpsActor extends Actor {
    * @param {string} type
    * @returns {number}
    */
+  // NOTE: migrated
   _sumeqt(dict, type, checkEquipped = false) {
     if (!dict) return 0.0
     let flt = (/** @type {string} */ str) => (!!str ? parseFloat(str) : 0)
@@ -649,6 +657,7 @@ export class GurpsActor extends Actor {
     return parseInt(sum * 100) / 100
   }
 
+  // NOTE: migrated
   _calculateWeights() {
     let data = this.system
     let eqt = data.equipment || {}
@@ -667,6 +676,7 @@ export class GurpsActor extends Actor {
     data.eqtsummary = eqtsummary
   }
 
+  // NOTE: migrated
   _calculateEncumbranceIssues() {
     const data = this.system
     const encs = data.encumbrance
@@ -714,10 +724,12 @@ export class GurpsActor extends Actor {
     }
   }
 
+  // NOTE: no longer needed
   _isEnhancedMove() {
     return !!this._getCurrentMoveMode()?.enhanced
   }
 
+  // NOTE: no longer needed
   _getSprintMove() {
     let current = this._getCurrentMoveMode()
     if (!current) return 0
@@ -725,6 +737,7 @@ export class GurpsActor extends Actor {
     return Math.floor(current.basic * 1.2)
   }
 
+  // NOTE: migrated
   _getCurrentMoveMode() {
     let move = this.system.move
     let current = Object.values(move).find(it => it.default)
@@ -845,6 +858,7 @@ export class GurpsActor extends Actor {
         }
   }
 
+  // NOTE: migrated
   _calculateRangedRanges() {
     if (!game.settings.get(Settings.SYSTEM_NAME, Settings.SETTING_CONVERT_RANGED)) return
     let st = +this.system.attributes.ST.value
@@ -867,6 +881,7 @@ export class GurpsActor extends Actor {
   }
 
   // Once all of the bonuses are applied, determine the actual level for each feature
+  // NOTE: not migrated, I'm not convinced this is ever actually needed
   _recalcItemFeatures() {
     let data = this.system
     this._collapseQuantumEq(data.melee, true)
--- module/item/data/spell.ts
+++ module/item/data/spell.ts
@@ -1,5 +1,7 @@
 import { CommonItemData, CommonItemDataSchema, ItemComponent, ItemComponentSchema } from './base.js'
 import fields = foundry.data.fields
+import { AnyObject } from 'fvtt-types/utils'
+import { makeRegexPatternFrom } from '../../../lib/utilities.js'
 
 class SpellData extends CommonItemData<SpellSchema> {
   static override defineSchema(): SpellSchema {
@@ -14,6 +16,34 @@ class SpellData extends CommonItemData<SpellSchema> {
   get component(): SpellComponent {
     return this.spl
   }
+
+  /* ---------------------------------------- */
+  /*  Data Preparation                        */
+  /* ---------------------------------------- */
+
+  override prepareBaseData(): void {
+    super.prepareBaseData()
+    this.component.level = this.component.import
+  }
+
+  /* ---------------------------------------- */
+
+  override applyBonuses(bonuses: AnyObject[]): void {
+    for (const bonus of bonuses) {
+      // Skills are affected by their base attribute changes
+      if (bonus.type === 'attribute') {
+        if (this.component.relativelevel.toUpperCase().startsWith((bonus.attrkey as string).toUpperCase())) {
+          this.component.level += bonus.mod as number
+        }
+      }
+
+      if (bonus.type === 'skill-spell' && bonus.isRanged) {
+        if (this.component.name.match(makeRegexPatternFrom(bonus.name as string, false))) {
+          this.component.level += bonus.mod as number
+        }
+      }
+    }
+  }
 }
 
 /* ---------------------------------------- */
@@ -25,6 +55,12 @@ class SpellComponent extends ItemComponent<SpellComponentSchema> {
       ...spellComponentSchema,
     }
   }
+
+  /* ---------------------------------------- */
+  /*  Derived Values                          */
+  /* ---------------------------------------- */
+
+  level: number = 0
 }
 
 /* ---------------------------------------- */
@@ -39,8 +75,10 @@ type SpellSchema = CommonItemDataSchema & typeof spellSchema
 
 const spellComponentSchema = {
   points: new fields.NumberField({ required: true, nullable: false }),
-  import: new fields.StringField({ required: true, nullable: false }),
-  level: new fields.NumberField({ required: true, nullable: false }),
+  // NOTE: change from previous schema where this was a string
+  import: new fields.NumberField({ required: true, nullable: false }),
+  // NOTE: no longer persistent data, always derived from import value
+  // level: new fields.NumberField({ required: true, nullable: false }),
   class: new fields.StringField({ required: true, nullable: false }),
   college: new fields.StringField({ required: true, nullable: false }),
   cost: new fields.StringField({ required: true, nullable: false }),
@@ -49,7 +87,7 @@ const spellComponentSchema = {
   resist: new fields.StringField({ required: true, nullable: false }),
   casttime: new fields.StringField({ required: true, nullable: false }),
   difficulty: new fields.StringField({ required: true, nullable: false }),
-  relativelevel: new fields.NumberField({ required: true, nullable: false }),
+  relativelevel: new fields.StringField({ required: true, nullable: false }),
   otf: new fields.StringField({ required: true, nullable: false }),
 }
 
