7b4068b7957b0049268631968c63460a675f2ba0
mjeffw <geek.macbeer@gmail.com>
2025-07-26 14:58:42 -0400

    Refactor PDF settings registration to use localized strings directly and use the new GurpsSettingsApplication.

--- module/pdf/settings.ts
+++ module/pdf/settings.ts
@@ -1,3 +1,4 @@
+import { GurpsSettingsApplication } from '../utilities/gurps-settings-application.js'
 import { MODULE_NAME, SETTING_BASICSET_PDF, SETTING_PDF_OPEN_FIRST } from './types.js'
 
 export function registerPDFSettings() {
@@ -6,22 +7,22 @@ export function registerPDFSettings() {
 
   // Support for combined or separate Basic Set PDFs
   game.settings.register(GURPS.SYSTEM_NAME, SETTING_BASICSET_PDF, {
-    name: game.i18n.localize('GURPS.settingBasicPDFs'),
-    hint: game.i18n.localize('GURPS.settingHintBasicPDFs'),
+    name: 'GURPS.settingBasicPDFs',
+    hint: 'GURPS.settingHintBasicPDFs',
     scope: 'world',
     config: false,
     type: String as any,
     choices: {
-      Combined: game.i18n.localize('GURPS.settingBasicPDFsCombined'),
-      Separate: game.i18n.localize('GURPS.settingBasicPDFsSeparate'),
+      Combined: 'GURPS.settingBasicPDFsCombined',
+      Separate: 'GURPS.settingBasicPDFsSeparate',
     },
     default: 'Combined',
     onChange: value => console.log(`Basic Set PDFs : ${value}`),
   })
 
   game.settings.register(GURPS.SYSTEM_NAME, SETTING_PDF_OPEN_FIRST, {
-    name: game.i18n.localize('GURPS.settingPDFOpenFirst'),
-    hint: game.i18n.localize('GURPS.settingHintPDFOpenFirst'),
+    name: 'GURPS.settingPDFOpenFirst',
+    hint: 'GURPS.settingHintPDFOpenFirst',
     scope: 'world',
     config: false,
     type: Boolean as any,
@@ -30,9 +31,9 @@ export function registerPDFSettings() {
   })
 
   game.settings.registerMenu(GURPS.SYSTEM_NAME, MODULE_NAME, {
-    name: game.i18n.localize('GURPS.pdf.settingsName'),
-    hint: game.i18n.localize('GURPS.pdf.settingsHint'),
-    label: game.i18n.localize('GURPS.pdf.settingsButton'),
+    name: 'GURPS.pdf.settingsName',
+    hint: 'GURPS.pdf.settingsHint',
+    label: 'GURPS.pdf.settingsButton',
     type: PDFSettingsApplication,
     restricted: false,
     icon: 'fa-solid fa-file-pdf',
@@ -47,58 +48,11 @@ export function getBasicSetPDFSetting(): string {
   return game.settings?.get(GURPS.SYSTEM_NAME, SETTING_BASICSET_PDF) as string
 }
 
-class PDFSettingsApplication extends foundry.applications.api.HandlebarsApplicationMixin(
-  foundry.applications.api.ApplicationV2
-) {
-  static override DEFAULT_OPTIONS = {
-    tag: 'form',
-    id: 'pdf-settings',
-    window: {
-      classes: ['standard-form'],
-      title: game.i18n?.localize('GURPS.pdf.settingsButton') ?? '',
-    },
-    position: {
-      width: 400,
-    },
-    form: {
-      handler: PDFSettingsApplication.#onSubmit,
-      closeOnSubmit: true,
-    },
-  }
-
-  static override PARTS = {
-    main: {
-      template: 'systems/gurps/templates/pdf/settings.hbs',
-    },
-    footer: {
-      template: 'templates/generic/form-footer.hbs',
-    },
-  }
-
-  override get title() {
-    return game.i18n!.localize(this.options.window.title)
-  }
-
-  protected override async _prepareContext(
-    options: foundry.applications.api.ApplicationV2.RenderOptions & { isFirstRender: boolean }
-  ): Promise<foundry.applications.api.ApplicationV2.RenderContext> {
-    const context = await super._prepareContext(options)
-
-    return foundry.utils.mergeObject(context, {
-      pdfOpenFirstSetting: game.settings!.get(GURPS.SYSTEM_NAME, SETTING_PDF_OPEN_FIRST),
-      basicSetPdfSetting: game.settings!.get(GURPS.SYSTEM_NAME, SETTING_BASICSET_PDF),
-      basicSetPdfChoices: {
-        Combined: game.i18n!.localize('GURPS.settingBasicPDFsCombined'),
-        Separate: game.i18n!.localize('GURPS.settingBasicPDFsSeparate'),
-      },
-      buttons: [{ type: 'submit', icon: 'fa-solid fa-save', label: 'SETTINGS.Save' }],
-    })
-  }
-
-  static async #onSubmit(event: SubmitEvent | Event, form: HTMLFormElement, formData: FormDataExtended): Promise<void> {
-    // @ts-expect-error: formData.object.basicSetPdfSetting may not be typed in Foundry's API
-    await game.settings!.set(GURPS.SYSTEM_NAME, SETTING_BASICSET_PDF, formData.object.basicSetPdfSetting)
-    // @ts-expect-error: formData.object.pdfOpenFirstSetting may not be typed in Foundry's API
-    await game.settings!.set(GURPS.SYSTEM_NAME, SETTING_PDF_OPEN_FIRST, formData.object.pdfOpenFirstSetting)
+class PDFSettingsApplication extends GurpsSettingsApplication {
+  constructor(options?: any) {
+    super(
+      { title: game.i18n!.localize('GURPS.pdf.settingsName'), module: MODULE_NAME, icon: 'fa-solid fa-file-pdf' },
+      options
+    )
   }
 }
