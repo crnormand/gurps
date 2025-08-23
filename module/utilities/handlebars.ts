e912604d1fbe97eacc9d5a491cf4a5371481bc0c
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2025-08-23 17:20:02 +0100

    Removed unused function buttonGroup

--- module/utilities/handlebars.ts
+++ module/utilities/handlebars.ts
@@ -1,45 +1,3 @@
-import fields = foundry.data.fields
-
-/**
- * Convert a DataField instance into an HTML button fragment.
- * @param {DataField} field             The DataField instance to convert to an input
- * @param {object} options              Helper options
- * @returns {Handlebars.SafeString}
- */
-function buttonGroup(field: fields.DataField.Any, options: any): Handlebars.SafeString {
-  const { classes, label, hint, rootId, stacked, units, hidden, widget, value, ...inputConfig } = options.hash
-  const groupConfig = {
-    label,
-    hint,
-    rootId,
-    stacked,
-    widget,
-    localize: inputConfig.localize,
-    units,
-    hidden,
-    classes: typeof classes === 'string' ? classes.split(' ') : [],
-  }
-  if (!field) {
-    console.error('Non-existent data field provided to {{formGroup}} handlebars helper.')
-    return new Handlebars.SafeString('')
-  }
-  try {
-    const group = field.toFormGroup(groupConfig, inputConfig)
-
-    const button = document.createElement('button')
-    button.type = 'button'
-    button.innerText = value
-    group.querySelector('input')?.replaceWith(button)
-
-    return new Handlebars.SafeString(group.outerHTML)
-  } catch (error) {
-    console.error(error)
-    return new Handlebars.SafeString('')
-  }
-}
-
-/* ---------------------------------------- */
-
 function print(value: any): void {
   console.log('**Handlebars Print**')
   console.log(value)
@@ -48,7 +6,6 @@ function print(value: any): void {
 /* ---------------------------------------- */
 
 export function registerGurpsHandlebarsHelpers() {
-  Handlebars.registerHelper('buttonGroup', buttonGroup)
   Handlebars.registerHelper('print', print)
 }
 
