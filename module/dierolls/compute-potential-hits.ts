611f606fe179c1c0cb8860c4db2d33e06e0a48be
mjeffw <geek.macbeer@gmail.com>
2026-01-05 16:13:07 -0500

    Add documentation to the computePotentialHits function.

--- module/dierolls/compute-potential-hits.ts
+++ module/dierolls/compute-potential-hits.ts
@@ -1,6 +1,15 @@
 // Regex to match shotgun rate of fire formats like '3x9', '2*5', etc.
 const SHOTGUN_ROF = /(?<rof>\d+)[×xX\*](?<projectiles>\d+)/
 
+/**
+ * Calclates potential hits based on weapon stats and margin of success, and returns data for chat message.
+ * @param optionalArgs.obj The weapon object containing rate of fire and recoil.
+ * @param optionalArgs.shots The number of shots fired.
+ * @param marginOfSuccess The margin of success from the attack roll.
+ * @param chatdata.rof The rate of fire text to display in chat.
+ * @param chatdata.rcl The recoil value of the weapon.
+ * @param chatdata.rofrcl The actual number of hits possible given the rate of fire and recoil.
+ */
 export function computePotentialHits(
   optionalArgs: { obj: { rcl: string; rof: string }; shots?: number },
   marginOfSuccess: number,
