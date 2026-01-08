6f7f7affe88d965b134d2c42329039d8a3ea4a30
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-01-08 20:23:41 +0000

    Moved extractOtfs to its own file, amended usage in handlebars helper and test suite

--- test/lib/extract-otfs.test.ts
+++ test/lib/extract-otfs.test.ts
@@ -1,5 +1,5 @@
-import { gurpslink } from '../../module/utilities/gurpslink.js'
 import { DamageTable } from '../../module/damage/damage-tables.js'
+import { extractOtfs } from '../../module/utilities/otf.ts'
 
 beforeAll(() => {
   globalThis.GURPS = {} as any
@@ -12,67 +12,6 @@ beforeAll(() => {
   }
   GURPS.DamageTables = new DamageTable()
 })
-
-const rollableTypes = [
-  'attribute',
-  'skill-spell',
-  'attack',
-  'attackdamage',
-  'weapon-parry',
-  'weapon-block',
-  'controlroll',
-  'roll',
-  'damage',
-  'derivedroll',
-  'deriveddamage',
-  'chat',
-]
-
-function extractOtfs(text: string) {
-  if (!text) return []
-  const actions = gurpslink(text, true) as any[]
-
-  return actions
-    .filter(action => action.action && rollableTypes.includes(action.action.type))
-    .map(action => {
-      let actionData = action.action
-      const dataActionMatch = action.text.match(/data-action='([^']+)'/)
-      if (dataActionMatch) {
-        actionData = JSON.parse(atob(dataActionMatch[1]))
-      }
-
-      let displayText = ''
-
-      if (actionData.overridetxt) {
-        displayText = actionData.overridetxt
-      } else if (
-        actionData.type === 'skill-spell' ||
-        actionData.type === 'attack' ||
-        actionData.type === 'attackdamage' ||
-        actionData.type === 'weapon-parry' ||
-        actionData.type === 'weapon-block'
-      ) {
-        displayText = actionData.name || ''
-      } else if (actionData.type === 'attribute') {
-        displayText = actionData.attribute || ''
-      } else if (actionData.type === 'controlroll') {
-        displayText = actionData.desc || ''
-      } else if (actionData.type === 'chat') {
-        displayText = 'Action'
-      }
-
-      let fullFormula = actionData.orig
-      if (actionData.overridetxt) {
-        fullFormula = `"${actionData.overridetxt}" ${actionData.orig}`
-      }
-
-      return {
-        formula: fullFormula,
-        text: displayText,
-        encodedAction: btoa(JSON.stringify(actionData)),
-      }
-    })
-}
 
 describe('extractOtfs', () => {
   describe('skill-spell type', () => {
