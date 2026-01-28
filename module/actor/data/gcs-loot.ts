4f97dcb64af9074b5b1caf30c5e2235012add667
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-01-28 22:31:41 +0000

    Added GCS Loot actor schema

---
+++ module/actor/data/gcs-loot.ts
@@ -0,0 +1,40 @@
+import { fields } from '../../types/foundry/index.ts'
+
+import { BaseActorModel } from './base.ts'
+
+const GcsLootVersion = 5
+
+/* ---------------------------------------- */
+
+class GcsLootModel extends BaseActorModel<GcsLootSchema> {
+  static override defineSchema(): GcsLootSchema {
+    return gcsLootSchema()
+  }
+}
+
+/* ---------------------------------------- */
+
+const gcsLootSchema = () => {
+  return {
+    // type LootData struct
+    // Version    int          `json:"version"`
+    // ID         tid.TID      `json:"id"`
+    // Name       string       `json:"name,omitzero"`
+    // Location   string       `json:"location,omitzero"`
+    // Session    string       `json:"session,omitzero"`
+    // ModifiedOn jio.Time     `json:"modified_date"`
+    // Equipment  []*Equipment `json:"equipment,omitzero"`
+    // Notes      []*Note      `json:"notes,omitzero"`
+    version: new fields.NumberField({ required: true, nullable: false, initial: GcsLootVersion }),
+    tid: new fields.DocumentIdField({ required: true, nullable: false, initial: foundry.utils.randomID }),
+    location: new fields.StringField({ required: true, nullable: false, initial: '' }),
+    sessionId: new fields.StringField({ required: true, nullable: false, initial: '' }),
+    modifiedOn: new fields.StringField({ required: true, nullable: false }),
+  }
+}
+
+type GcsLootSchema = ReturnType<typeof gcsLootSchema>
+
+/* ---------------------------------------- */
+
+export { GcsLootModel }
