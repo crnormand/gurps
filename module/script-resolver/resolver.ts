85f5d790b326713d0f07f85c65ff1e6a6dc2106e
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-01-30 14:36:21 +0000

    Completed basic functionality of scriptResolver

---
+++ module/script-resolver/resolver.ts
@@ -0,0 +1,134 @@
+import { executeScript } from './execute-script.ts'
+import { ScriptAttribute } from './script-attribute.ts'
+import { ScriptCharacter } from './script-character.ts'
+import { BaseProvider, GLOBAL_RESOLVER_CACHE, ResolverCacheKey, ScriptArgument, SelfProvider } from './types.ts'
+
+/* ---------------------------------------- */
+
+class Resolver {
+  static maximumAllowedResolutionDepth = 20
+
+  static maximumScriptExecutionTimeMs = 1000
+
+  static resolveToNumber<Provider extends BaseProvider>(
+    actor: Actor.Implementation | null,
+    selfProvider: SelfProvider<Provider>,
+    text: string
+  ): number {
+    text = text.trim()
+
+    if (text === '') return 0
+
+    let value = Number(text)
+
+    if (!isNaN(value)) return value
+
+    const result = this.resolveScript(actor, selfProvider, text)
+
+    value = Number(result)
+
+    if (isNaN(value)) {
+      console.error(`Resolver: Unable to resolve text to number: ${text}`)
+
+      return 0
+    }
+
+    return value
+  }
+
+  /* ---------------------------------------- */
+
+  static async resolveScript<Provider extends BaseProvider>(
+    actor: Actor.Implementation | null,
+    selfProvider: SelfProvider<Provider>,
+    text: string,
+    resolutionDepth = 0
+  ): Promise<string> {
+    resolutionDepth += 1
+
+    try {
+      if (resolutionDepth > Resolver.maximumAllowedResolutionDepth) {
+        console.error('Maximum resolution depth reached!')
+
+        return ''
+      }
+
+      // NOTE: Temporary. The full implementation would use a global resolve cache.
+      let resolverCache: Map<ResolverCacheKey, string> = this.globalResolverCache
+
+      if (actor && actor.isOfType('gcsCharacter')) {
+        resolverCache = actor.system.resolverCache
+      }
+
+      const key: ResolverCacheKey = { id: selfProvider.id, text: text }
+
+      if (resolverCache.has(key)) {
+        return resolverCache.get(key)!
+      }
+
+      const maxTime = this.maximumScriptExecutionTimeMs
+
+      const args: ScriptArgument[] = []
+
+      if (selfProvider.provider) {
+        args.push({
+          name: 'self',
+          value: selfProvider.provider,
+        })
+      }
+
+      if (actor && actor.isOfType('gcsCharacter')) {
+        args.push({
+          name: 'entity',
+          value: { ...new ScriptCharacter(actor.system) },
+        })
+
+        const list = actor.system._attributes
+
+        for (const attribute of list) {
+          const def = attribute.definition
+
+          if (!def || def.isSeparator) continue
+
+          args.push({
+            name: '$' + attribute.id,
+            value: () => {
+              return new ScriptAttribute(attribute)
+            },
+          })
+        }
+      }
+
+      console.log(text, args, maxTime)
+      const result = await executeScript(text, args, maxTime)
+
+      console.log('result', result)
+
+      if (result.success === false) {
+        console.error('Script execution failed. Reason:', result.reason)
+
+        return result.value
+      }
+
+      resolverCache.set(key, result.value)
+
+      return result.value
+    } catch (error) {
+      console.error(`Script execution failed: ${text}`, error)
+
+      return ''
+    } finally {
+      resolutionDepth -= 1
+    }
+  }
+
+  /* ---------------------------------------- */
+
+  static get globalResolverCache(): Map<ResolverCacheKey, string> {
+    if (!game.settings) return new Map()
+
+    return game.settings.get(GURPS.SYSTEM_NAME, GLOBAL_RESOLVER_CACHE)
+  }
+}
+
+export { Resolver }
