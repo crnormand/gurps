6b386579fd8813126caa1942c901c68419289bb3
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-01-31 14:55:12 +0000

    checkpoint: moving from using workers to custom interpreter

--- module/script-resolver/providers.ts
+++
@@ -1,9 +0,0 @@
-import { BaseProvider } from './types.ts'
-
-interface ActorProvider extends BaseProvider {
-  name: string
-}
-
-/* ---------------------------------------- */
-
-export type { ActorProvider }
