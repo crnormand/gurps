abfcf1bed7d5f2e5501f9ffb1390eee3e11242c6
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-02-02 17:17:56 +0000

    Fixed invalid type errors for Features

--- module/features/weapon-bonus.ts
+++ module/features/weapon-bonus.ts
@@ -57,4 +57,216 @@ type WeaponBonusSchema = BaseFeatureSchema & ReturnType<typeof weaponBonusSchema
 
 /* ---------------------------------------- */
 
-export { WeaponBonus }
+/**
+ * The below classes are essentially stubs but need to be declared due to the TypedPseudoDocument data validation
+ * requiring a match in the TYPE accessor for each class.
+ */
+
+class WeaponAccBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponAccBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponScopeAccBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponScopeAccBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponDRDivisorBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponDRDivisorBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponEffectiveSTBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponEffectiveSTBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponMinSTBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponMinSTBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponMinReachBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponMinReachBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponMaxReachBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponMaxReachBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponHalfDamageRangeBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponHalfDamageRangeBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponMinRangeBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponMinRangeBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponMaxRangeBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponMaxRangeBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponRecoilBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponRecoilBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponBulkBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponBulkBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponParryBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponParryBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponBlockBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponBlockBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponRofMode1ShotsBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponRofMode1ShotsBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponRofMode1SecondaryBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponRofMode1SecondaryBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponRofMode2ShotsBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponRofMode2ShotsBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponRofMode2SecondaryBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponRofMode2SecondaryBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponNonChamberShotsBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponNonChamberShotsBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponChamberShotsBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponChamberShotsBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponShotDurationBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponShotDurationBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponReloadTimeBonus extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponReloadTimeBonus
+  }
+}
+
+/* ---------------------------------------- */
+
+class WeaponSwitch extends WeaponBonus {
+  static override get TYPE(): FeatureType {
+    return FeatureType.WeaponSwitch
+  }
+}
+
+export {
+  WeaponBonus,
+  WeaponAccBonus,
+  WeaponScopeAccBonus,
+  WeaponDRDivisorBonus,
+  WeaponEffectiveSTBonus,
+  WeaponMinSTBonus,
+  WeaponMinReachBonus,
+  WeaponMaxReachBonus,
+  WeaponHalfDamageRangeBonus,
+  WeaponMinRangeBonus,
+  WeaponMaxRangeBonus,
+  WeaponRecoilBonus,
+  WeaponBulkBonus,
+  WeaponParryBonus,
+  WeaponBlockBonus,
+  WeaponRofMode1ShotsBonus,
+  WeaponRofMode1SecondaryBonus,
+  WeaponRofMode2ShotsBonus,
+  WeaponRofMode2SecondaryBonus,
+  WeaponNonChamberShotsBonus,
+  WeaponChamberShotsBonus,
+  WeaponShotDurationBonus,
+  WeaponReloadTimeBonus,
+  WeaponSwitch,
+}
