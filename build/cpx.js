80f9c6b601e5456aa35fcdeaf14b9ed302eaf0a4
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-02-09 09:19:10 +0000

    stopped vendor files being tsc processed

--- build/cpx.js
+++ build/cpx.js
@@ -5,10 +5,23 @@ const staticFolders = ['assets', 'icons', 'lang', 'templates', 'ui']
 const staticFiles = ['changelog.md', 'LICENSE.txt', 'README.md', 'system.json', 'template.json']
 const outputBase = 'dist'
 
+// Copy vendor-style libraries verbatim (do not run through tsc).
+// These are loaded by Foundry as classic scripts / pre-bundled modules.
+const sourceFolders = [{ input: 'src/lib/**/*', output: `${outputBase}/lib` }]
+
 // Copy static folders to the output directory
 for (const folder of staticFolders) {
   const input = `static/${folder}/**/*`
   const output = `${outputBase}/${folder}`
+  if (process.argv.includes('--watch')) {
+    cpx.watch(input, output, {})
+  } else {
+    cpx.copy(input, output, {})
+  }
+}
+
+// Copy source folders (not under static/)
+for (const { input, output } of sourceFolders) {
   if (process.argv.includes('--watch')) {
     cpx.watch(input, output, {})
   } else {
