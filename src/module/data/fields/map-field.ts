a2d6dd6d3dffe43404df73bf579227e05d359c3b
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-02-10 08:10:04 +0000

    Moved MapField namespace above class declaration

--- src/module/data/fields/map-field.ts
+++ src/module/data/fields/map-field.ts
@@ -1,6 +1,140 @@
 import { DataModel, fields } from '@gurps-types/foundry/index.js'
 import { AnyMutableObject, AnyObject, SimpleMerge } from 'fvtt-types/utils'
 
+namespace MapField {
+  export type Any = MapField<fields.DataField.Any, fields.DataField.Any, AnyOptions>
+
+  /* ---------------------------------------- */
+
+  type BaseAssignmentType<AssignmentKeyType, AssignmentValueType> =
+    | Record<string, { key: AssignmentKeyType; value: AssignmentValueType }>
+    | Iterable<[AssignmentKeyType, AssignmentValueType]>
+    | [AssignmentKeyType, AssignmentValueType][]
+    | [AssignmentKeyType, AssignmentValueType]
+
+  /* ---------------------------------------- */
+
+  export type ElementFieldType<
+    KeyType extends fields.DataField.Any,
+    ValueType extends fields.DataField.Any,
+  > = fields.SchemaField<{ key: KeyType; value: ValueType }>
+
+  /* ---------------------------------------- */
+
+  export type Options<AssignmentKeyType, AssignmentValueType> = fields.ArrayField.Options<
+    BaseAssignmentType<AssignmentKeyType, AssignmentValueType>
+  >
+
+  /* ---------------------------------------- */
+
+  export type AnyOptions = Options<unknown, unknown>
+
+  /* ---------------------------------------- */
+
+  // NOTE: Currently, there is no difference between this and ArrayField.DefaultOptions
+  // so we can get away with just passing through the same type.
+  export type DefaultOptions = fields.ArrayField.DefaultOptions
+
+  /* ---------------------------------------- */
+
+  type MergedOptions<Opts extends AnyOptions> = SimpleMerge<DefaultOptions, Opts>
+
+  /* ---------------------------------------- */
+
+  export type _EffectiveOptions<Options extends AnyOptions> = Options['initial'] extends undefined
+    ? Options['min'] extends 0
+      ? SimpleMerge<Options, { initial: [] }>
+      : Options // If `min` is set to anything but `0` the effective `initial` of `[]` is invalid.
+    : Options
+
+  /* ---------------------------------------- */
+
+  export type AssignmentKeyType<KeyType extends fields.DataField.Any> = KeyType extends
+    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_assignment_data': infer AssignmentData })
+    | { ' __fvtt_types_internal_assignment_data': infer AssignmentData }
+    ? AssignmentData
+    : never
+
+  export type AssignmentValueType<ValueType extends fields.DataField.Any> = ValueType extends
+    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_assignment_data': infer AssignmentData })
+    | { ' __fvtt_types_internal_assignment_data': infer AssignmentData }
+    ? AssignmentData
+    : never
+
+  export type AssignmentElementType<AssignmentKeyType, AssignmentValueType> =
+    | { key: AssignmentKeyType; value: AssignmentValueType }
+    | [AssignmentKeyType, AssignmentValueType]
+
+  /* ---------------------------------------- */
+
+  export type InitializedKeyType<KeyType extends fields.DataField.Any> = KeyType extends
+    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_initialized_data': infer InitializedData })
+    | { ' __fvtt_types_internal_initialized_data': infer InitializedData }
+    ? InitializedData
+    : never
+
+  export type InitializedValueType<ValueType extends fields.DataField.Any> = ValueType extends
+    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_initialized_data': infer InitializedData })
+    | { ' __fvtt_types_internal_initialized_data': infer InitializedData }
+    ? InitializedData
+    : never
+
+  export type InitializedElementType<InitializedKeyType, InitializedValueType> =
+    | { key: InitializedKeyType; value: InitializedValueType }
+    | [InitializedKeyType, InitializedValueType]
+
+  /* ---------------------------------------- */
+
+  export type PersistedKeyType<KeyType extends fields.DataField.Any> = KeyType extends
+    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_source_data': infer PersistedData })
+    | { ' __fvtt_types_internal_source_data': infer PersistedData }
+    ? PersistedData
+    : never
+
+  export type PersistedValueType<ValueType extends fields.DataField.Any> = ValueType extends
+    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_source_data': infer PersistedData })
+    | { ' __fvtt_types_internal_source_data': infer PersistedData }
+    ? PersistedData
+    : never
+
+  export type PersistedElementType<PersistedKeyType, PersistedValueType> =
+    | [PersistedKeyType, PersistedValueType]
+    | { key: PersistedKeyType; value: PersistedValueType }
+
+  /* ---------------------------------------- */
+
+  export type AssignmentType<
+    AssignmentKeyType,
+    AssignmentValueType,
+    Opts extends AnyOptions,
+  > = fields.DataField.DerivedAssignmentType<
+    BaseAssignmentType<AssignmentKeyType, AssignmentValueType>,
+    MapField._EffectiveOptions<MergedOptions<Opts>>
+  >
+
+  /* ---------------------------------------- */
+
+  export type InitializedType<
+    InitializedKeyType,
+    InitializedValueType,
+    Opts extends AnyOptions,
+  > = fields.DataField.DerivedInitializedType<
+    Map<InitializedKeyType, InitializedValueType>,
+    MapField._EffectiveOptions<MergedOptions<Opts>>
+  >
+
+  export type PersistedType<
+    PersistedKeyType,
+    PersistedValueType,
+    Opts extends AnyOptions,
+  > = fields.DataField.DerivedInitializedType<
+    [PersistedKeyType, PersistedValueType][],
+    MapField._EffectiveOptions<MergedOptions<Opts>>
+  >
+}
+
+/* ---------------------------------------- */
+
 class MapField<
   const KeyType extends fields.DataField.Any,
   const ValueType extends fields.DataField.Any,
@@ -272,140 +406,6 @@ class MapField<
       map.set(entryKey, entryValue)
     }
   }
-}
-
-/* ---------------------------------------- */
-
-namespace MapField {
-  export type Any = MapField<fields.DataField.Any, fields.DataField.Any, AnyOptions>
-
-  /* ---------------------------------------- */
-
-  type BaseAssignmentType<AssignmentKeyType, AssignmentValueType> =
-    | Record<string, { key: AssignmentKeyType; value: AssignmentValueType }>
-    | Iterable<[AssignmentKeyType, AssignmentValueType]>
-    | [AssignmentKeyType, AssignmentValueType][]
-    | [AssignmentKeyType, AssignmentValueType]
-
-  /* ---------------------------------------- */
-
-  export type ElementFieldType<
-    KeyType extends fields.DataField.Any,
-    ValueType extends fields.DataField.Any,
-  > = fields.SchemaField<{ key: KeyType; value: ValueType }>
-
-  /* ---------------------------------------- */
-
-  export type Options<AssignmentKeyType, AssignmentValueType> = fields.ArrayField.Options<
-    BaseAssignmentType<AssignmentKeyType, AssignmentValueType>
-  >
-
-  /* ---------------------------------------- */
-
-  export type AnyOptions = Options<unknown, unknown>
-
-  /* ---------------------------------------- */
-
-  // NOTE: Currently, there is no difference between this and ArrayField.DefaultOptions
-  // so we can get away with just passing through the same type.
-  export type DefaultOptions = fields.ArrayField.DefaultOptions
-
-  /* ---------------------------------------- */
-
-  type MergedOptions<Opts extends AnyOptions> = SimpleMerge<DefaultOptions, Opts>
-
-  /* ---------------------------------------- */
-
-  export type _EffectiveOptions<Options extends AnyOptions> = Options['initial'] extends undefined
-    ? Options['min'] extends 0
-      ? SimpleMerge<Options, { initial: [] }>
-      : Options // If `min` is set to anything but `0` the effective `initial` of `[]` is invalid.
-    : Options
-
-  /* ---------------------------------------- */
-
-  export type AssignmentKeyType<KeyType extends fields.DataField.Any> = KeyType extends
-    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_assignment_data': infer AssignmentData })
-    | { ' __fvtt_types_internal_assignment_data': infer AssignmentData }
-    ? AssignmentData
-    : never
-
-  export type AssignmentValueType<ValueType extends fields.DataField.Any> = ValueType extends
-    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_assignment_data': infer AssignmentData })
-    | { ' __fvtt_types_internal_assignment_data': infer AssignmentData }
-    ? AssignmentData
-    : never
-
-  export type AssignmentElementType<AssignmentKeyType, AssignmentValueType> =
-    | { key: AssignmentKeyType; value: AssignmentValueType }
-    | [AssignmentKeyType, AssignmentValueType]
-
-  /* ---------------------------------------- */
-
-  export type InitializedKeyType<KeyType extends fields.DataField.Any> = KeyType extends
-    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_initialized_data': infer InitializedData })
-    | { ' __fvtt_types_internal_initialized_data': infer InitializedData }
-    ? InitializedData
-    : never
-
-  export type InitializedValueType<ValueType extends fields.DataField.Any> = ValueType extends
-    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_initialized_data': infer InitializedData })
-    | { ' __fvtt_types_internal_initialized_data': infer InitializedData }
-    ? InitializedData
-    : never
-
-  export type InitializedElementType<InitializedKeyType, InitializedValueType> =
-    | { key: InitializedKeyType; value: InitializedValueType }
-    | [InitializedKeyType, InitializedValueType]
-
-  /* ---------------------------------------- */
-
-  export type PersistedKeyType<KeyType extends fields.DataField.Any> = KeyType extends
-    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_source_data': infer PersistedData })
-    | { ' __fvtt_types_internal_source_data': infer PersistedData }
-    ? PersistedData
-    : never
-
-  export type PersistedValueType<ValueType extends fields.DataField.Any> = ValueType extends
-    | (abstract new (...args: infer _1) => { ' __fvtt_types_internal_source_data': infer PersistedData })
-    | { ' __fvtt_types_internal_source_data': infer PersistedData }
-    ? PersistedData
-    : never
-
-  export type PersistedElementType<PersistedKeyType, PersistedValueType> =
-    | [PersistedKeyType, PersistedValueType]
-    | { key: PersistedKeyType; value: PersistedValueType }
-
-  /* ---------------------------------------- */
-
-  export type AssignmentType<
-    AssignmentKeyType,
-    AssignmentValueType,
-    Opts extends AnyOptions,
-  > = fields.DataField.DerivedAssignmentType<
-    BaseAssignmentType<AssignmentKeyType, AssignmentValueType>,
-    MapField._EffectiveOptions<MergedOptions<Opts>>
-  >
-
-  /* ---------------------------------------- */
-
-  export type InitializedType<
-    InitializedKeyType,
-    InitializedValueType,
-    Opts extends AnyOptions,
-  > = fields.DataField.DerivedInitializedType<
-    Map<InitializedKeyType, InitializedValueType>,
-    MapField._EffectiveOptions<MergedOptions<Opts>>
-  >
-
-  export type PersistedType<
-    PersistedKeyType,
-    PersistedValueType,
-    Opts extends AnyOptions,
-  > = fields.DataField.DerivedInitializedType<
-    [PersistedKeyType, PersistedValueType][],
-    MapField._EffectiveOptions<MergedOptions<Opts>>
-  >
 }
 
 /* ---------------------------------------- */
