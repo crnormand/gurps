36e9b43138d13dfa56c61d5aa987b5dc50d8587f
mjeffw <geek.macbeer@gmail.com>
2026-02-18 14:31:53 -0500

    select-energy-pool

--- module/gurps.js
+++ module/gurps.js
@@ -1566,72 +1566,94 @@ if (!globalThis.GURPS) {
    * If the desc contains *Cost ?FP or *Max:9 then perform action
    * @param {GurpsActor|User} actor
    * @param {string} desc
+   * @returns {null|number} an overriding MAX value if *Max is found, otherwise null.
    */
   async function applyModifierDesc(actor, desc) {
     if (!desc) return null
-    let m = desc.match(COSTS_REGEX)
+    let match = desc.match(COSTS_REGEX)
 
-    if (!!m && !!actor && !actor.isSelf) {
-      let delta = parseInt(m.groups.cost)
-      let target = m.groups.type
+    // Handle *Costs modifiers.
+    if (!!match && !!actor && !actor.isSelf) {
+      let delta = parseInt(match.groups.cost)
+      let target = match.groups.type
+
       if (target.match(/^[hf]p/i)) {
-        let k = target.toUpperCase()
-        // @ts-ignore
-        delta = actor.system[k].value - delta
-        await actor.update({ ['system.' + k + '.value']: delta })
+        let key = target.toUpperCase()
+
+        // @ts-expect-error - dynamic property access on actor.system
+        delta = actor.system[key].value - delta
+        await actor.update({ ['system.' + key + '.value']: delta })
       } else if (target.match(/^tr/i)) {
+        // TODO Today this is using the chat command -- I think it should directly update the actor.
         await GURPS.ChatProcessors.startProcessingLines('/setEventFlags true false false\\\\/' + target + ' -' + delta) // Make the tracker command quiet
-        return null
       } else {
         // Build a list of possible costs based on the actor's HP, FP, and Trackers, and prompt the user to select one.
         const costs = {}
+
         if (actor.system.HP) costs[`system.HP.value`] = `${game.i18n.localize('GURPS.HP')} (${actor.system.HP.value})`
         if (actor.system.FP) costs[`system.FP.value`] = `${game.i18n.localize('GURPS.FP')} (${actor.system.FP.value})`
+
         for (const [key, tracker] of Object.entries(actor.system.additionalresources?.tracker ?? {})) {
           costs[`system.additionalresources.tracker.${key}.value`] = `${tracker.name} (${tracker.value})`
         }
 
-        // Unknown cost type: prompt the user for either HP, FP, or a Tracker name.
-        // Create a DialogV2 to prompt the user for the cost type as a radio button list.
-        const response = await foundry.applications.api.DialogV2.wait({
-          window: { title: game.i18n.localize('GURPS.selectEnergyPool') },
-          content: `
-          <label for="costType">${game.i18n.localize('GURPS.selectEnergyPoolDetail')}
-            <select id="costType" name="costType">
-              ${Object.entries(costs)
-                .map(
-                  ([value, label]) =>
-                    `<option value="${value}" ${value === `system.FP.value` ? 'selected' : ''}>${label}</option>`
-                )
-                .join('')}
-            </select>
-          </label>`,
-          buttons: [
-            {
-              action: 'ok',
-              label: 'GURPS.ok',
-              callback: (_event, button, _dialog) => {
-                const costType = button.form.elements.costType.value
-                return costType
-              },
-            },
-          ],
-        })
-
-        if (!response) {
-          // Show a warning notification to the users that the cost was not applied.
+        // If there are no valid costs, show a warning notification to the users that the cost was not applied.
+        if (Object.keys(costs).length === 0) {
           ui.notifications.warn(game.i18n.format('GURPS.costNotApplied', { points: delta }))
-          return null
-        }
+        } else {
+          // Unknown cost type: prompt the user for either HP, FP, or a Tracker name.
+          // Create a DialogV2 to prompt the user for the cost type using a dropdown (select) input.
+          const defaultCostKey = costs[`system.FP.value`] ? 'system.FP.value' : Object.keys(costs)[0]
+          const response = await foundry.applications.api.DialogV2.wait({
+            window: { title: game.i18n.localize('GURPS.selectEnergyPool') },
+            content: `
+            <label for="costType">${game.i18n.localize('GURPS.selectEnergyPoolDetail')}
+              <select id="costType" name="costType" style="width: fit-content;">
+                ${Object.entries(costs)
+                  .map(([value, label]) => {
+                    const escValue = foundry.utils.escapeHTML(value)
+                    const escLabel = foundry.utils.escapeHTML(label)
+                    const selected = value === defaultCostKey ? ' selected' : ''
 
-        await actor.update({ [response]: foundry.utils.getProperty(actor, response) - delta })
+                    return `<option value="${escValue}"${selected}>${escLabel}</option>`
+                  })
+                  .join('')}
+              </select>
+            </label>`,
+            buttons: [
+              {
+                action: 'ok',
+                label: 'GURPS.ok',
+                callback: (_event, button, _dialog) => {
+                  return button.form.elements.costType.value
+                },
+              },
+              {
+                action: 'cancel',
+                label: 'GURPS.cancel',
+                callback: () => {
+                  return null
+                },
+              },
+            ],
+          })
+
+          if (!response) {
+            // Show a warning notification to the users that the cost was not applied.
+            ui.notifications.warn(game.i18n.format('GURPS.costNotApplied', { points: delta }))
+          } else {
+            await actor.update({ [response]: foundry.utils.getProperty(actor, response) - delta })
+          }
+        }
       }
     }
 
     let parse = desc.replace(/.*\*max: ?(\d+).*/gi, '$1')
+
     if (parse != desc) {
       return parseInt(parse)
     }
+
     return null // indicating no overriding MAX value
   }
   GURPS.applyModifierDesc = applyModifierDesc
