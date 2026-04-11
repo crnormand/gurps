bf55ef328c0c1cc52c65132665c7846d91457d33
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-04-11 01:30:28 +0100

    lint: auto-fixes

--- src/module/actor/actor-sheet.js
+++ src/module/actor/actor-sheet.js
@@ -12,8 +12,8 @@ import { GgaContextMenuV2 } from '../ui/context-menu.js'
 
 import { Advantage, Equipment, Melee, Modifier, Note, Ranged, Reaction, Skill, Spell } from './actor-components.js'
 import { ActorImporter } from './actor-importer.js'
-import { prepareTrackerDataForSheet } from './sheets/modern/dialog-crud-handler.ts'
 import MoveModeEditor from './move-mode-editor.js'
+import { prepareTrackerDataForSheet } from './sheets/modern/dialog-crud-handler.ts'
 import SplitDREditor from './splitdr-editor.js'
 import { ActorType } from './types.js'
 
