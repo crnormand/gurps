25284daea8b15d68612638f846350adf5ffb63ca
mjeffw <geek.macbeer@gmail.com>
2026-05-06 16:54:56 -0400

    fix: ensure damage application works for linked tokens and handle null token cases

--- module/token-actions.js
+++ module/token-actions.js
@@ -63,6 +63,7 @@ export class TokenActions {
     if (!token) {
       const tokenDocument = await actor.getTokenDocument()
       token = tokenDocument.object
+      if (!token) return null
     }
 
     const tokenActions = new TokenActions(token)
