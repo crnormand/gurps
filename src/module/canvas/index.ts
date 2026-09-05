3f984339937d23344f49ff80298b9a6fcf8412e4
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-09-05 18:38:48 -0230

    feat(canvas): Add B239 Measured Template alternate radius calculation setting

--- src/module/canvas/index.ts
+++ src/module/canvas/index.ts
@@ -1,12 +1,17 @@
 import type { GurpsModule } from '@gurps-types/gurps-module.js'
 
+import { GurpsRegion } from './region.js'
 import { GurpsRuler } from './ruler.js'
+import { registerSettings } from './settings.js'
 
 function init() {
   console.log('GURPS | Initializing GURPS Canvas Module')
 
   Hooks.once('init', () => {
+    registerSettings()
+
     CONFIG.Canvas.rulerClass = GurpsRuler
+    CONFIG.Region.objectClass = GurpsRegion
   })
 }
 
