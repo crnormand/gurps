9e104099def8412e2c0962bc24bb4963d7a230ab
mjeffw <geek.macbeer@gmail.com>
2026-09-16 14:57:48 -0400

    Remove unnecessary whitespace in npc-input.js

--- src/module/util/npc-input.js
+++ src/module/util/npc-input.js
@@ -65,7 +65,6 @@ const POSSIBLE_ATTRIBUTE_KEYS = {
   advantages: true,
 }
 
-
 // TODO: Convert to ApplicationV2
 export class NpcInput extends FormApplication {
   constructor(actor, options = {}) {
